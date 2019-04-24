0b196d69dd2db3a8b8d35f0e2c3ece13cfaddf3e
Gareth Jones <oxidising@gmail.com>
2019-04-24 12:40:55 +0100

    More tidying of functions for processing of CRDS data

--- data_processing/process_crds.py
+++ data_processing/process_crds.py
@@ -3,6 +3,7 @@ import os
 import sys
 import json
 import xarray
+import glob
 import pandas as np
 
 class proc_CRDS:
@@ -27,7 +28,8 @@ class proc_CRDS:
         except FileNotFoundError as err:
             print("Cannot open ", self.site_params, err)
 
-
+    # def crds_data_read(data_file) 
+    # From process_gcwerks.py
     def read_data_file(self, data_file):
         """ This function reads in a single CRDS datafile
             and creates a Pandas dataframe to store the data
@@ -38,7 +40,7 @@ class proc_CRDS:
                 tuple (xarray.Dataset, list): Dataset of processed data,
                 list of species if found
         
-        """   
+        """
 
         # Lists to hold the header and species data
         header = []
@@ -59,20 +61,14 @@ class proc_CRDS:
             header.append(df_header[i][1].upper())
 
         # Read in the data
-        df = pd.read_csv(data_file
-                        skiprows=4,
-                        header = None,
-                        sep=r"\s+",
-                        names = header,
-                        dtype = {"DATE": str, "TIME": str})
+        df = pd.read_csv(data_file, skiprows=4, header = None, sep=r"\s+", 
+                        names = header, dtype = {"DATE": str, "TIME": str})
                         
+
+        # TODO - GJ - I don't like this - tidy it up somehow?
         # Interpret time
-        time = [dt(2000 + int(date[0:2]),
-                        int(date[2:4]),
-                        int(date[4:]),
-                        int(time[0:2]),
-                        int(time[2:4]),
-                        int(time[4:]))
+        time = [dt(2000 + int(date[0:2]), int(date[2:4]), int(date[4:]), int(time[0:2]), int(time[2:4]), int(time[4:]))
+
                 for date, time in zip(df["DATE"].values, df["TIME"].values)]
         
         df.index = time
@@ -85,27 +81,130 @@ class proc_CRDS:
         return ds, species
 
 
-    def read_data(self, data_folder):
-        """ This function collects raw 
+    def search_data_files(self, data_folder, site, search_string):
+        """ Searches for data files in data_folder for the site
+            using search_string
+
             Args:
-                data_folder (str): The directory holding data files
-            Returns:
-                
+                data_folder (str): Data folder to search
+                site (str): Site to use
+                search_string (str): Search string to find files
+                Example: ".*.1minute.*.dat"
+            Returns
+                list: List of matching files in data_folder
 
         """
-        # TODO - get this properly
-        site = "yahyah"
+
+        file_search = os.path.join(data_folder, site.lower() + search_string)
+        data_files = glob.glob(file_search)
+        return data_files
+
+    def populate_data_list(self, data_folder, site, search_string):
+        """ This function populates a list of data files to use and
+            a list of inlets
+
+            Args:
+                data_folder (str): The directory holding data files
+                search_string (str): Search string to find files
+                Example: ".*.1minute.*.dat"
+            Returns:
+                None           
+        """
+
         params_crds = self.params["CRDS"]
+
         site_string = params_crds[site]["gcwerks_site_name"]
 
-        # String type to search folder for
-        data_file_search = join(data_folder, site.lower() + ".*.1minute.*.dat")
-        # Glob the files 
-        data_files = glob.glob(data_file_search)
-        inlets = [f.split(".")[-2] for f in data_files]
+        # 2019-04-24
+        # I'll get rid of this for now, I don't like this being hard coded in
+        # data_dir = params_crds["directory"].replace("%site, site_string")
 
-        for i, inlet in enumerate(inlets):
-            # 
+        # search_string = ".*.1minute.*.dat"
+        self.data_files = self.search_data_files(data_folder, site, search_string)
+
+        # Find the inlets
+        self.inlets = [f.split(".")[-2] for f in data_files]
+
+    def process_data(self):
+        """ Process the list of data files in data_files
+            and create a list of Pandas dataframes
+
+            Returns:
+                None
+        """
+        # A dataset for each species
+        species_datasets = []
+
+        for i, inlet in enumerate(self.inlets):
+
+            # Create the pandas dataframe
+            ds, species = self.read_data_file(data_file[i])
+
+            # Write a NetCDF file for each species
+            for sp in species:
+                # Species specific dataset
+                species_ds = ds[[sp, sp + " variability", sp + " number_of_observations"]]
+                species_ds = species_ds.dropna("time")
+
+            global_attributes = params_crds[site]["global_attributes"]
+            global_attributes["inlet_height_magl"] = float(inlet[0:-1])
+            global_attributes["comment"] = params_crds["comment"]
+
+            species_ds = attributes(species_ds, sp, site.upper(),
+                               global_attributes=global_attributes,
+                               scale=scales[sp],
+                               sampling_period=60,
+                               date_range=date_range)
+
+            
+            if len(species_ds.time.values) == 0:
+                # Then must have not passed date_range filter?
+                print(" ... no data in range")
+                # then do nothing
+            else:
+                species_datasets.append(species_ds)
+
+        
+        return species_datasets
+
+        
+    def write_files(self, data_list, output_folder):
+        """ Writes the data in data_list to file
+
+            Args:
+                data_list (list): A list of xarray datasets
+                output_folder (str): Folder to write NetCDFs
+            Returns:
+                None
+            Raises:
+
+
+                            
+        """
+        for d in data_list:
+            if not isinstance(d, xarray.Dataset):
+                raise TypeError("Passed list must contain only xarray.Dataset objects")
+
+        
+
+        # Write file
+        nc_filename = output_filename(output_folder,
+                                        network,
+                                        "CRDS",
+                                        site.upper(),
+                                        ds_sp.time.to_pandas().index.to_pydatetime()[
+                                            0],
+                                        ds_sp.species,
+                                        inlet=inlet,
+                                        version=version)
+
+        print("Writing " + nc_filename)
+        ds_sp.to_netcdf(nc_filename)
+        print("... written.")
+
+
+            
+                
 
         
 
