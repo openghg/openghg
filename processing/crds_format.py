85eba1f836866fd79bbcc2cc10af4e7cf0c3c4d4
Gareth Jones <oxidising@gmail.com>
2019-05-13 14:45:40 +0100

    File format class

---
+++ processing/crds_format.py
@@ -0,0 +1,60 @@
+class CRDS:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def create(datasources, start_datetime, end_datetime):
+        # Create some CRDS
+
+    @staticmethod
+    def read_file(filename):
+        # Parse filename data
+        datasources = _get_datasources_filename(filename)
+
+        c = CRDS()
+        c._datasources = datasources
+
+        # INsert metadata parse stuff here
+
+        # Columns of data -  get on with it!
+
+        data = _read_file(filename)
+
+        if(len(data) != len(datasources):
+            raise IOError("Something is terribly wrong...")
+
+        
+        for i in range(0, len(data)):
+            # This add_data function adds it to the object store
+            datasources[i].add_data(data[i])
+
+        c._start_datetime = data[0].earliest_datetime()
+
+        c._end_datetime = data[0].latest_datetime()
+
+        return c
+
+
+    def write_file(self, filename):
+        data = [] 
+        datetimes = {}
+        for datasource in self._datasources:
+            # Get datas
+            d = datasource.get_data(start=self._start_datetime, end=self._end_datetime)
+            data.append(d)
+
+            for datetime in d.datetimes_in_data():
+                datetimes[datetime] = 1
+        
+        datetimes = list(datetimes.keys())
+
+        datetimes.sort()
+
+        with open(filename, "w") as FILE:
+            FILE.write(metadata)
+            # Merge the dataframes
+            # If no data for that datetime set as NaN
+            # Write these combined tables to the file
+
+
+        
