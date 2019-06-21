739f85846911163a7cccbfd1956a756932c129bf
Gareth Jones <oxidising@gmail.com>
2019-06-21 11:58:45 +0100

    Test changes

--- test/mock/hugs/test_hello.py
+++ test/mock/hugs/test_hello.py
@@ -3,6 +3,8 @@
 import pytest
 import os
 
+from Acquire.Client import Drive, StorageCreds
+
 from hugs.Client import Hello
 
 @pytest.fixture(scope="session")
@@ -12,6 +14,22 @@ def tempdir(tmpdir_factory):
 
 
 def test_hello(authenticated_user, tempdir):
+
+    print(tempdir)
+
+    drive_name = "hugs_files"
+
+    creds = StorageCreds(user=authenticated_user, service_url="storage")
+
+    drive = Drive(name=drive_name, creds=creds)
+
+    filemeta = drive.upload(filename=__file__, uploaded_name="test.py")
+
+    print(filemeta.__dict__)
+
+    location = filemeta.location()
+
+    print(location.to_string())
 
     hello = Hello(service_url="hugs")
 
