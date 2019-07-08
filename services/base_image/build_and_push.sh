639dd6674338b4aedaa0735dd070a273bb174d23
Christopher Woods <chryswoods@gmail.com>
2019-07-08 16:10:59 +0000

    Added in the correct values to configure the hugs service

--- services/base_image/build_and_push.sh
+++ services/base_image/build_and_push.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-rsync -a --verbose ../../Acquire . --exclude '__pycache__'
+rsync -a --verbose ../../hugs . --exclude '__pycache__'
 rsync -a --verbose ../admin . --exclude '__pycache__'
 
-docker build -t chryswoods/acquire-base:latest .
+docker build -t chryswoods/hugs-base:latest .
 
-rm -rf Acquire admin
-
-#docker push chryswoods/acquire-base:latest
-
-#docker run --rm -it chryswoods/acquire-base:latest
+rm -rf hugs admin
--- services/hugs_service/credentials/upload_credentials.py
+++ services/hugs_service/credentials/upload_credentials.py
@@ -23,14 +23,14 @@ data = {}
 data["user"] = "ocid1.user.oc1..aaaaaaaas2elik6wbuvvab5qeewdj5dkzs3kyi3w3jqhkgbmjub272oyfmda"
 
 # Fingerprint for the login keyfile
-data["fingerprint"] = "1d:75:2d:85:06:ed:e3:7e:52:56:a2:5e:7e:d6:c6:3f"
+data["fingerprint"] = "72:8b:ab:c6:78:19:f8:6e:ef:cd:2f:7e:95:cb:a0:b7"
 
 # The keyfile itself - we will now read the file and pull it into text
 keyfile = sys.argv[1]
 data["key_lines"] = open(sys.argv[1],"r").readlines()
 
 # The tenancy in which this user and everything exists!
-data["tenancy"] = "ocid1.tenancy.oc1..aaaaaaaa3eiex6fbfj626uwhs3dg24oygknrhhgfj4khqearluf4i74zdt2a"
+data["tenancy"] = "ocid1.tenancy.oc1..aaaaaaaaadarqjzv7bxmb3scckyhmqlqyok3dfqenmzyirrbpjv32uyy74ca"
 
 # The passphrase to unlock the key - VERY SECRET!!!
 data["pass_phrase"] = sys.argv[2]
@@ -47,7 +47,7 @@ secret_config["LOGIN"] = data
 ## that will store all data related to logging into accounts
 
 data = {}
-data["compartment"] = "ocid1.compartment.oc1..aaaaaaaat33j7w74mdyjenwoinyeawztxe7ri6qkfbm5oihqb5zteamvbpzq"
+data["compartment"] = "ocid1.compartment.oc1..aaaaaaaawwwf4eocadm6suukafuemsxqfk74frirbmzskptqa4rkrwumtnpa"
 data["bucket"] = "hugs_principal"
 
 secret_config["BUCKET"] = data
