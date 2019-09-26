1f69ccf6c344bc055e260eb26fee813127281a26
Gareth Jones <oxidising@gmail.com>
2019-09-26 16:47:42 +0100

    Adding emissions/footprint processing

---
+++ HUGS/Modules/_footprint.py
@@ -0,0 +1,18 @@
+""" Module to load emissions maps and break them down into usable chunks
+    for saving as Datasources
+
+    
+"""
+class Footprint:
+    _footprint_root = "footprint"
+    _footprint_uuid = "8cba4797-510c-foot-print-e02a5ee57489"
+    
+    def __init__():
+        self.
+
+
+    def process():
+        """ For a single footprint file we can break it down into chunks of a certain size
+            for easier lookup.
+
+        """
