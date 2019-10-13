aae1e829a0d0694fb709f2b551cffc595ce2e9a9
Gareth Jones <oxidising@gmail.com>
2019-10-13 19:37:52 +0100

    Forgot notes on search module

--- HUGS/Interface/_search.py
+++ HUGS/Interface/_search.py
@@ -1,5 +1,8 @@
 __all__ = ["get_map_locations", "search_emissions", "get_emissions_data"]
 
+"""
+    Placeholders for interaction with emissions module
+"""
 def search_emissions(search_terms):
     return ["WAO-20magl_EUROPE_201501", "WAO-20magl_EUROPE_201502", "WAO-20magl_EUROPE_201503", "WAO-20magl_EUROPE_201504"]
 
