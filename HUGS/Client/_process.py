ce3441590e1634d7da4c3086553bf149786b62db
Gareth Jones <oxidising@gmail.com>
2019-10-15 15:49:01 +0100

    WIP: Error wth /tmp on server, compression problem?

--- HUGS/Client/_process.py
+++ HUGS/Client/_process.py
@@ -106,6 +106,7 @@ class Process:
                     source_name = os.path.splitext(filename)[0]
                 
                 filemeta = drive.upload(file[0])
+                filemeta._compression = None
                 par = PAR(location=filemeta.location(), user=user)
                 par_secret = hugs.encrypt_data(par.secret())
 
@@ -122,6 +123,7 @@ class Process:
                 filename = str(file).split("/")[-1]
                 
                 filemeta = drive.upload(file)
+                filemeta._compression = None
                 par = PAR(location=filemeta.location(), user=user)
                 par_secret = hugs.encrypt_data(par.secret())
 
