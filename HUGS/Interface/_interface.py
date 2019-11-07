5616643a4060e042ea8ffe9ea5578397de52fe99
Gareth Jones <oxidising@gmail.com>
2019-11-07 15:19:25 +0000

    WIP: Each stage working, need to tidy

--- HUGS/Interface/_interface.py
+++ HUGS/Interface/_interface.py
@@ -2,6 +2,7 @@
     A class to create the interface for HUGS
     
 """
+import bqplot as bq
 from Acquire.Client import User
 import collections
 from datetime import datetime
@@ -9,6 +10,7 @@ import functools
 import ipywidgets as widgets
 from HUGS.Client import Retrieve, Search
 from HUGS.Interface import generate_password
+import numpy as np
 import pandas as pd
 
 
@@ -33,10 +35,19 @@ class Interface:
     def __init__(self):
         self._base_url = "https://hugs.acquire-aaai.com/t"
         self._search_results = None
+        # This is the order in which they'll be shown (if created)
         self._module_list = ["register", "login", "search", "download", "plot_1", "plot_2"]
         # Maybe just made _widgets a defaultdict(list) as originally thought?
         # self._widgets = {key: [] for key in self._module_list} #collections.defaultdict(list)
-        self._widgets = {}
+        # self._widgets = {} 
+        self._widgets = collections.defaultdict(widgets.VBox)
+        # self.populate_dictionary(self._module_list)
+        # self._widgets = {}
+        self.table_style = {'description_width': 'initial'}
+        self.table_layout = {'width': '100px', 'min_width': '100px','height': '28px', 'min_height': '28px'}
+        self.date_layout = {'width': '275px', 'min_width': '200px','height': '28px', 'min_height': '28px'}
+        self.checkbox_layout = {'width': '100px', 'min_width': '100px','height': '28px', 'min_height': '28px'}
+        self.statusbar_layout = {'width': '250px', 'min_width': '250px', 'height': '28px', 'min_height': '28px'}
 
     def create_login(self, user, x):
         """ Create a basic login widget and add it to the widget dict
@@ -44,7 +55,7 @@ class Interface:
         """
         return False
     
-    def create_user(self):
+    def create_registration_box(self):
         """
             User creation box
 
@@ -80,7 +91,7 @@ class Interface:
             Create a login box
 
             Returns:
-                tuple (User, ipywidgets.VBox): 
+                tuple (User, list): Acquire.User and a list of ipywidgets
         """
         login_text = widgets.HTML(value="<b>Please click the buton below to create a login link</b>")
         username_text = widgets.Text(value=None, placeholder="username", description="Username: ")
@@ -98,13 +109,15 @@ class Interface:
                 print(username_text.value)
                 response = user.request_login()
 
-            if user.wait_for_login():
-                status_text.value = f"<font color='green'>Login success</font>"
-            else:
-                status_text.value = f"<font color='red'>Login failure</font>"
+            # if user.wait_for_login():
+            #     status_text.value = f"<font color='green'>Login success</font>"
+            # else:
+            #     status_text.value = f"<font color='red'>Login failure</font>"
 
         login_button.on_click(login)
-        return user, widgets.VBox(children=[username_text, login_button, status_text, login_link_box])
+        login_widgets = [username_text, login_button, status_text, login_link_box]
+        # return user, widgets.VBox(children=[username_text, login_button, status_text, login_link_box])
+        return user, login_widgets
 
     def create_search_box(self):
         """ Create the searching interface
@@ -127,7 +140,7 @@ class Interface:
         search_children = [search_terms, locations, start_picker, end_picker, data_type,
                         search_button, status_box]
 
-        search_vbox = widgets.VBox(children=search_children)
+        # search_vbox = widgets.VBox(children=search_children)
 
         def call_search(x):
             if start_picker.value:
@@ -151,11 +164,9 @@ class Interface:
             if search_results:
                 date_keys = self.parse_results(results=search_results)
                 status_box.value = f"<font color='green'>Success</font>"
-                # Now we have search results we can select the ones we want to download
-                # TODO - move this so it's a part of the widgets dict
                 d_box = self.create_download_box(date_keys=date_keys)
-                # Update the children of the previous box to include the download box
-                search_vbox.children = search_children + d_box
+                # Add the download widgets to the download box
+                self.add_widgets(section="download", _widgets=d_box)
             else:
                 status_box.value = f"<font color='red'>No results</font>"
 
@@ -211,7 +222,9 @@ class Interface:
 
         # return download_widgets
 
-        return widgets.VBox(children=[search_vbox])
+        # return widgets.VBox(children=[search_vbox])
+
+        return search_children
     
     def create_download_box(self, date_keys):
         """ Creates the plotting box that holds the plotting buttons and windows
@@ -221,16 +234,11 @@ class Interface:
             Returns:
                 list: List of download widgets
         """
-        table_style = {'description_width': 'initial'}
-        table_layout = {'width': '100px', 'min_width': '100px', 'height': '28px', 'min_height': '28px'}
-        date_layout = {'width': '275px', 'min_width': '200px', 'height': '28px', 'min_height': '28px'}
-        checkbox_layout = {'width': '100px', 'min_width': '100px', 'height': '28px', 'min_height': '28px'}
-        statusbar_layout = {'width': '250px', 'min_width': '250px', 'height': '28px', 'min_height': '28px'}
-
-        header_label_site = widgets.HTML(value=f"<b>Site</b>", layout=table_layout)
-        header_label_gas = widgets.HTML(value=f"<b>Gas</b>", layout=table_layout)
-        header_label_dates = widgets.HTML(value=f"<b>Dates</b>", layout=date_layout)
-        header_label_select = widgets.HTML(value=f"<b>Select</b>", layout=checkbox_layout)
+        
+        header_label_site = widgets.HTML(value=f"<b>Site</b>", layout=self.table_layout)
+        header_label_gas = widgets.HTML(value=f"<b>Gas</b>", layout=self.table_layout)
+        header_label_dates = widgets.HTML(value=f"<b>Dates</b>", layout=self.date_layout)
+        header_label_select = widgets.HTML(value=f"<b>Select</b>", layout=self.checkbox_layout)
 
         checkbox_objects = []
         search_keys = []
@@ -245,14 +253,14 @@ class Interface:
             search_keys.append(key)
 
             dates = date_keys[key]["dates"].replace("_", " to ").replace("T", " ")
-            date_label = widgets.Label(value=dates, layout=date_layout)
+            date_label = widgets.Label(value=dates, layout=self.date_layout)
 
             split_key = key.split("_")
             site_name = split_key[0].upper()
             gas_name = split_key[1].upper()
 
-            gas_label = widgets.Label(value=gas_name, layout=table_layout)
-            site_label = widgets.Label(value=site_name, layout=table_layout)
+            gas_label = widgets.Label(value=gas_name, layout=self.table_layout)
+            site_label = widgets.Label(value=site_name, layout=self.table_layout)
 
             date_labels.append(date_label)
             site_labels.append(site_label)
@@ -269,10 +277,13 @@ class Interface:
 
         dynamic_box = widgets.HBox(children=[site_vbox, gas_vbox, dates_vbox, checkbox_vbox])
 
-        download_button = widgets.Button(description="Download", button_style="success", layout=table_layout)
+        download_button = widgets.Button(description="Retrieve", button_style="success", layout=self.table_layout)
 
-        self._widgets["status_bar"] = widgets.HTML(value="Status: Waiting...", layout=statusbar_layout)
-        status_bar = self._widgets["status_bar"]
+        # self._widgets["status_bar"] = widgets.HTML(value="Status: Waiting...", layout=statusbar_layout)
+        # status_bar = self._widgets["status_bar"]
+        status_bar = widgets.HTML(value="Status: Waiting...", layout=self.statusbar_layout)
+        # self._status_bar = status_bar
+        self.add_widgets(section="download_status", _widgets=status_bar)
 
         def on_download_click(a):
             download_keys = {key: date_keys[key]["keys"] for key in arg_dict if arg_dict[key].value is True}
@@ -281,14 +292,15 @@ class Interface:
         download_button.on_click(on_download_click)
         download_button_box = widgets.HBox(children=[download_button])
 
-        plot_box = widgets.VBox()
+        # plot_box = widgets.VBox()
 
         # TODO - can build each GUI with a dictionary of lists that are used to create each block of widgets
 
         # Put widgets we want to be able to interact with from outside in self._widgets dictionary
         # Should we put all widgets in? May get messy if these all need naming etc
-        self._widgets["download"] = [header_box, dynamic_box, download_button_box, status_bar, plot_box]
-        download_widgets = self._widgets["download"]
+        # self._widgets["download"] = [header_box, dynamic_box, download_button_box, status_bar, plot_box]
+        # download_widgets = self._widgets["download"]
+        download_widgets = [header_box, dynamic_box, download_button_box, status_bar]
 
         return download_widgets
 
@@ -317,9 +329,104 @@ class Interface:
 
         return date_keys
 
+
+    def create_plotting_box(self, data):
+        """ Create the window for plotting the downloaded data
+
+            TODO
+            1. Implement adding new plots to create a grid
+            2. Editing of plots ?
+            3. Adding new overlays to axes
+        """
+        # Create some checkboxes
+        plot_checkboxes = []
+        plot_keys = []
+
+        for key in data:
+            # Create a more readable description
+            desc = " ".join(key.split("_")).upper()
+            plot_keys.append(key)
+            plot_checkboxes.append(widgets.Checkbox(description=desc, value=False))
+
+
+        select_instruction = widgets.HTML(value="<b>Select data: </b>", layout=self.table_layout)
+        plot_button = widgets.Button(description="Plot", button_style="success", layout=self.table_layout)
+
+        select_box = widgets.HBox(children=[select_instruction])
+        checkbox_box = widgets.VBox(children=plot_checkboxes)
+        horiz_select = widgets.HBox(children=[select_box, checkbox_box])
+        plot_box = widgets.HBox(children=[plot_button])
+        ui_box = widgets.VBox(children=[horiz_select, plot_box])
+
+        # Clunk
+        arg_dict = {plot_keys[i]: checkbox for i, checkbox in enumerate(plot_checkboxes)}
+
+        # def on_download_click(a):
+        #     download_keys = {key: date_keys[key]["keys"]
+        #                      for key in arg_dict if arg_dict[key].value is True}
+        #     self.download_data(download_keys=download_keys)
+
+        selected_data = []
+
+        def select_data(**kwargs):
+            selected_data.clear()
+
+            for key in kwargs:
+                if kwargs[key] is True:
+                    selected_data.append(key)
+
+            print(selected_data)
+
+        # Link the checkbox selection with the selected dataframes to plot
+        out = widgets.interactive_output(select_data, arg_dict)
+
+        output = widgets.Output()
+
+        def plot_data(arg):
+            """ Each key in the data dict is a dataframe
+
+            """
+            # Here take the keys in the selected data list and use them to
+            # access the Dataframes to plot
+            # Use the same axes. Can have a button to create new plots etc in the future
+
+            # TODO - change this to take the data directly from the dict?
+            plot_data = [data[x] for x in selected_data]
+
+            # For now just plot the first column in the data
+
+            # Setup the axes
+            x_scale = bq.DateScale()
+            y_scale = bq.LinearScale()
+            scales = {"x": x_scale, "y": y_scale}
+
+            lines.x = [d.index.values.tolist() for d in plot_data]
+            lines.y = [d.iloc[:, 0] for d in plot_data]
+
+        x_scale = bq.DateScale()
+        y_scale = bq.LinearScale()
+        scales = {"x": x_scale, "y": y_scale}
+
+        ax = bq.Axis(label="Date", scale=x_scale)
+        # TODO - this could be updated depending on what's being plot
+        ay = bq.Axis(label="Count", scale=y_scale, orientation="vertical")
+
+        lines = bq.Lines(x=np.arange(100), y=np.cumsum(np.random.randn(2, 100), axis=1), scales=scales)
+        figure = bq.Figure(marks=[lines], axes=[ax, ay], animation_duration=1000)
+
+        plot_button.on_click(plot_data)
+
+        # plotting_box.children = [ui_box, out, figure]
+        return [ui_box, out, figure]
+
     # Will this force an update ?
-    def update_statusbar(self, text):
-        self._widgets["status_bar"].value = f"Status: {text}"
+    def update_statusbar(self, status_name, text):
+
+        # if not isinstance(self._widgets[status_name], widgets.widget_string.HTML):
+        #     print(type(self._widgets[status_name]))
+        #     raise TypeError("This function can only be used to update status bars that are ipywidgets HTML objects")
+        return False
+        # self._widgets[status_name].value = f"Status: {text}"
 
     def download_data(self, download_keys):
         """ Download the data in the selected keys from the object store
@@ -329,59 +436,78 @@ class Interface:
             Returns:
                 None
         """
-        # retrieve = Retrieve(service_url=self.base_url)
-        # data = retrieve.retrieve(keys=download_keys)
-        self.update_statusbar("Downloading...")
+        retrieve = Retrieve(service_url=self._base_url)
+        data = retrieve.retrieve(keys=download_keys)
+        
+        # TODO - get the status bar updates working
+        # self.update_statusbar(status_name="download_status", text="Downloading...")
 
-        # # Convert the JSON into Dataframes
-        # for key in data:
-        #     data[key] = pd.read_json(data[key])
+        # Convert the JSON into Dataframes
+        for key in data:
+            data[key] = pd.read_json(data[key])
 
-        # # Update the status bar
-        # if data:
-        #     update_statusbar("Download complete")
-        #     # Create the plotting box
-        #     create_plotting_box()
-        # else:
-        #     update_statusbar("No data downloaded")
+        # Update the status bar
+        if data:
+            # update_statusbar("Download complete")
+            # Create the plotting box
+            # self.add_widgets self.create_plotting_box()
+            self.add_widgets(section="plot_1", _widgets=self.create_plotting_box(data=data))
+        else:
+            raise NotImplementedError
+            # update_statusbar("No data downloaded")
         
         # Messy
-        self._widgets["download"][-1].children = [widgets.Button(description="Register", button_style="primary")]
+        # self._widgets["download"][-1].children = [widgets.Button(description="Register", button_style="primary")]
 
-    def populate_dictionary(self, keys):
-        """ Populate the dictionary used to store the lists of widgets
+    # def populate_dictionary(self, keys):
+    #     """ Populate the dictionary used to store the lists of widgets
+
+    #         Args:
+    #             keys (list): List of keys to be used to populate dictionary
+    #         Returns:
+    #             None
+    #     """
+    #     # Create a VBox for each of the sections of the interface
+    #     # We can then update the children for each section
+    #     # TODO - not sure if this needs to be a function, move to ctor
+    #     # if this is all it'll do
+    #     self._widgets = {key: widgets.VBox() for key in keys}
+
+    def add_widgets(self, section, _widgets):
+        """ Add widgets to be the children of the key section
+            in the widget dictionary
 
             Args:
-                keys (list): List of keys to be used to populate dictionary
-            Returns:
+                section (str): Section / module of the GUI to assign these
+                widgets to
+                _widgets (widget / list): List of widgets
+            Returns:    
                 None
         """
-        self._widgets = {key: [] for key in keys}
+        if not isinstance(_widgets, list):
+            _widgets = [_widgets]
 
+        self._widgets[section].children = _widgets
 
     def show_interface(self, new_user=False):
         """ Return the completed interface
 
         """
-        # widget_list = []
-
+        # Here we can assign children to the VBox's created in the function above
         if new_user:
             # widget_list.append(self.create_user())
-
-            self._widgets["create"] = self.create_user()
+            self.add_widgets(section="register", _widgets=self.create_registration_box())
 
         user, login_box = self.create_login_box()
+        self.add_widgets(section="login", _widgets=login_box)
 
-        self._widgets["login_box"] = login_box
+        self.add_widgets(section="search", _widgets=self.create_search_box())
 
-        self._widgets["search_box"] = self.create_search_box()
-
-        # How best to get the dict to talk to each other?
-        # Can refer to functions in the 
-
-        box = widgets.VBox(children=list(self._widgets.values()))
+        # box = widgets.VBox(children=list(self._widgets.values()))
+        # This creates a list of of VBoxes to be placed in the main VBox
+        boxes = [self._widgets[b] for b in self._module_list]
+        box = widgets.VBox(children=boxes)
         
-
         return box
 
 
