e1ca7b5373d70875db82b5f3ae03a0feddc24f1a
Gareth Jones <oxidising@gmail.com>
2019-12-06 17:12:45 +0000

    Further Datasource tests

--- test/Modules/test_datasource.py
+++ test/Modules/test_datasource.py
@@ -1,7 +1,9 @@
 import datetime
 import os
+from pathlib import Path
 import pytest
 import uuid
+import numpy as np
 import pandas as pd
 import xarray
 import zarr
@@ -12,7 +14,7 @@ from Acquire.ObjectStore import string_to_encoded
 
 from HUGS.ObjectStore import get_dated_object
 from HUGS.ObjectStore import get_dated_object_json
-from HUGS.Modules import Datasource
+from HUGS.Modules import Datasource, CRDS
 from HUGS.ObjectStore import get_local_bucket
 
 mocked_uuid = "00000000-0000-0000-00000-000000000000"
@@ -26,7 +28,12 @@ def data():
     test_data = "../data/proc_test_data/CRDS"
     filepath = os.path.join(dir_path, test_data, filename)
 
-    return pd.read_csv(filepath, header=None, skiprows=1, sep=r"\s+")
+    filepath = Path(filepath)
+
+    crds = CRDS.load()
+    combined_data = crds.read_data(data_filepath=filepath)
+    
+    return combined_data
 
 @pytest.fixture
 def datasource():
@@ -51,27 +58,51 @@ def mock_uuid2(monkeypatch):
     monkeypatch.setattr(uuid, 'uuid4', mock_uuid)
 
 
-# @pytest.fixture(scope="session")
-# def save_data_in_store(mock_uuid):
-#     bucket = local_bucket.get_local_bucket()
-#     datasource = get_datasources(raw_data=data)[0]
-#     original_slice = datasource._data.head(1)
+def test_add_data(data):
+    d = Datasource.create(name="test") 
 
-#     # data = Datasource.create(name="test_name", instrument="test_instrument", site="test_site",
-#     #                          network="test_network", data=datasource._data)
-#     # data.save(bucket)
+    metadata = data["ch4"]["metadata"]
+    ch4_data = data["ch4"]["data"]
 
-#     print("Save data in store uuid : ", datasource._uuid)
+    assert ch4_data["ch4 count"].iloc[0] == pytest.approx(1960.24)
+    assert ch4_data["ch4 stdev"].iloc[0] == pytest.approx(0.236)
+    assert ch4_data["ch4 n_meas"].iloc[0] == pytest.approx(26.0)
 
-#     datasource.save(bucket)
+    d.add_data(metadata=metadata, data=ch4_data, data_type="CRDS")
+
+    assert d._data[0][0]["ch4 count"].iloc[0] == ch4_data["ch4 count"].iloc[0]
+    assert d._data[0][0]["ch4 stdev"].iloc[0] == ch4_data["ch4 stdev"].iloc[0]
+    assert d._data[0][0]["ch4 n_meas"].iloc[0] == ch4_data["ch4 n_meas"].iloc[0]
+
+    datasource_metadata = d.metadata()
+
+    assert datasource_metadata['data_type'] == 'timeseries'
+    assert datasource_metadata['height'] == '248m'
+    assert datasource_metadata['instrument'] == 'picarro'
+    assert datasource_metadata['port'] == '8'
+    assert datasource_metadata['site'] == 'bsd'
+    assert datasource_metadata['source_name'] == 'test'
+    assert datasource_metadata['species'] == 'ch4'
+
+def test_get_dataframe_daterange():
+    n_days = 100
+    epoch = datetime.datetime(1970,1,1,1,1)
+    random_data = pd.DataFrame(data=np.random.randint(0, 100, size=(100, 4)), 
+                    index=pd.date_range(epoch, epoch + datetime.timedelta(n_days-1), freq='D'), columns=list('ABCD'))
+
+    d = Datasource.create(name="test")
+
+    start, end = d.get_dataframe_daterange(random_data)
+
+    assert start == pd.Timestamp("1970-01-01 01:01:00+0000")
+    assert end == pd.Timestamp("1970-04-10 01:01:00+0000")
+
 
 def test_save(mock_uuid2):
     bucket = get_local_bucket()
 
     datasource = Datasource.create(name="test_name", instrument="test_instrument", site="test_site", network="test_network")
-
     datasource.add_metadata(key="data_type", value="timeseries")
-
     datasource.save(bucket)
 
     prefix = "%s/uuid/%s" % (Datasource._datasource_root, datasource._uuid)
