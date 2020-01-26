9912d445de1661488e9647200e341999012c62c0
Gareth Jones <oxidising@gmail.com>
2020-01-26 15:38:53 +0000

    Final notebooks

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hugs",
-    version="0.0.2",
+    version="0.0.3",
     author="Gareth Jones",
     author_email="g.m.jones@bristol.ac.uk",
     description="A HUb for greenhouse Gas Data Science",
