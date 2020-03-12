2d01473f41e5b5d65cc1380ed853739d7fb022ed
Gareth Jones <oxidising@gmail.com>
2020-03-12 17:19:26 +0000

    WIP: PAR progress

--- HUGS/Data/job_controllers/bc4_template.py
+++ HUGS/Data/job_controllers/bc4_template.py
@@ -32,29 +32,27 @@ def run():
     job_name = job_data["job_name"]
     script_file = job_data["script_filename"]
     par_data = job_data["par"]
-
-    rands = [random.randint(0,10) for _ in range(50)]
-
-    print(job_name)
     
     with open("some_rands.txt", "w") as f:
         f.write(str(rands))
 
-    # # Make the job folders at the location of this file
-    # job_path = Path(__file__).resolve().parent.joinpath(job_name)
+    # Make the job folders at the location of this file
+    job_path = Path(__file__).resolve().parent.joinpath(job_name)
 
-    # folders = ["input", "output", "logs"]
-    # for f in folders:
-    #     fpath = job_path.joinpath(f)
-    #     fpath.mkdir(parents=True)
+    folders = ["input", "output", "logs"]
+    for f in folders:
+        fpath = job_path.joinpath(f)
+        fpath.mkdir(parents=True)
 
-    # # Use the PAR (pre-authenticated request) to access the cloud drive
-    # # where the input data has been uploaded (if needed) and output data will 
-    # # be stored
-    # par = PAR.from_data(par_data)
-    # par_drive = par.resolve()
+    # Use the PAR (pre-authenticated request) to access the cloud drive
+    # where the input data has been uploaded (if needed) and output data will 
+    # be stored
+    par = PAR.from_data(par_data)
+    par_drive = par.resolve()
 
-    # files = par_drive.list_files(dir="input")
+    files = par_drive.list_files(dir="input")
+
+    print(files)
 
 
 if __name__ == "__main__":
