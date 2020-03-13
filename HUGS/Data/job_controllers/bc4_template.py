36bec66ef669d521bcba7d3cbc2bbbcca68fa7c3
Gareth Jones <oxidising@gmail.com>
2020-03-13 15:51:58 +0000

    Drive upload

--- HUGS/Data/job_controllers/bc4_template.py
+++ HUGS/Data/job_controllers/bc4_template.py
@@ -32,25 +32,31 @@ def run():
     job_name = job_data["job_name"]
     script_file = job_data["script_filename"]
     par_data = job_data["par"]
-    
+    par_secret = job_data["par_secret"]
+
+    rands = [random.random() for _ in range(100)]
+
     with open("some_rands.txt", "w") as f:
         f.write(str(rands))
 
     # Make the job folders at the location of this file
-    job_path = Path(__file__).resolve().parent.joinpath(job_name)
+    # job_path = Path(__file__).resolve().parent.joinpath(job_name)
 
-    folders = ["input", "output", "logs"]
-    for f in folders:
-        fpath = job_path.joinpath(f)
-        fpath.mkdir(parents=True)
+    # folders = ["input", "output", "logs"]
+    # for f in folders:
+    #     fpath = job_path.joinpath(f)
+    #     fpath.mkdir(parents=True)
 
     # Use the PAR (pre-authenticated request) to access the cloud drive
     # where the input data has been uploaded (if needed) and output data will 
     # be stored
-    par = PAR.from_data(par_data)
-    par_drive = par.resolve()
+    par = PAR.from_data(data=par_data)
+    
+    drive = par.resolve(secret=par_secret)
 
-    files = par_drive.list_files(dir="input")
+    drive.upload("some_rands.txt")
+
+    files = drive.list_files()
 
     print(files)
 
