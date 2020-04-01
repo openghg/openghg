ff4682666eb8ae7981c44c50da287fba504ff8f6
Gareth Jones <oxidising@gmail.com>
2020-04-01 16:19:15 +0100

    Remove relative import and remove unnecessary code

--- services/base_image/fixed/fast_oci__init__.py
+++ services/base_image/fixed/fast_oci__init__.py
@@ -29,11 +29,13 @@ BaseClient = _lazy_import.lazy_class("oci.base_client.BaseClient")
 Request = _lazy_import.lazy_class("oci.request.Request")
 Signer = _lazy_import.lazy_class("oci.signer.Signer")
 
-from .version import __version__  # noqa
+# from .version import __version__  # noqa
 
 wait_until = _lazy_import.lazy_function("oci.waiter.wait_until")
 
 __all__ = [
-    "BaseClient", "Error", "Request", "Response", "Signer", "config", "constants", "decorators", "exceptions", "regions", "wait_until", "pagination", "auth", "retry",
-    "audit", "container_engine", "core", "database", "dns", "email", "file_storage", "identity", "key_management", "load_balancer", "object_storage", "resource_search"
+    "BaseClient", "Error", "Request", "Response", "Signer", "config", "constants", 
+    "decorators", "exceptions", "regions", "wait_until", "pagination", "auth", "retry",
+    "audit", "container_engine", "core", "database", "dns", "email", "file_storage", 
+    "identity", "key_management", "load_balancer", "object_storage", "resource_search"
 ]
