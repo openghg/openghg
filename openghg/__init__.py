9b4158477b140a596e0508d1afc70d0736b72cb7
Gareth Jones <oxidising@gmail.com>
2020-10-06 14:39:55 +0100

    WIP: Adding in documentation

--- openghg/__init__.py
+++ openghg/__init__.py
@@ -4,5 +4,14 @@
 """
 from . import client, jobs, localclient, modules, objectstore, processing, service, util
 from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+
+v = get_versions()
+
+print(v)
+
+__version__ = v['version']
+__branch__ = v['branch']
+__repository__ = v['repository']
+__revisionid__ = v['full-revisionid']
+
+del v, get_versions
