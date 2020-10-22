add3c5033f2c57ceb45d72f04102c082c52b458a
Gareth Jones <oxidising@gmail.com>
2020-10-22 18:29:18 +0100

    Fix for barbados inlet error

--- openghg/modules/_gcwerks.py
+++ openghg/modules/_gcwerks.py
@@ -228,25 +228,28 @@ class GCWERKS:
 
         # Read inlets from the parameters dictionary
         expected_inlets = self.get_inlets(site_code=site)
-        # Get the inlets in the dataframe
-        try:
-            data_inlets = data["Inlet"].unique().tolist()
-        except KeyError:
-            raise KeyError(
-                "Unable to read inlets from data, please ensure this data is of the GC \
-                                    type expected by this processing module"
-            )
 
-        # For now just add air to the expected inlets
-        expected_inlets.append("air")
+        if len(expected_inlets) == 1 and expected_inlets[0] == "any":
+            matching_inlets = expected_inlets
+        else:
+            # Get the inlets in the dataframe
+            try:
+                data_inlets = data["Inlet"].unique().tolist()
+            except KeyError:
+                raise KeyError(
+                    "Unable to read inlets from data, please ensure this data is of the GC type expected by this processing module"
+                )
 
-        matching_inlets = [data_inlet for data_inlet in data_inlets for inlet in expected_inlets if fnmatch(data_inlet, inlet)]
+            # For now just add air to the expected inlets
+            expected_inlets.append("air")
 
-        if not matching_inlets:
-            raise ValueError(
-                "Inlet mismatch - please ensure correct site is selected. \
-                                Mismatch between inlet in data and inlet in parameters file."
-            )
+            matching_inlets = [data_inlet for data_inlet in data_inlets for inlet in expected_inlets if fnmatch(data_inlet, inlet)]
+
+            if not matching_inlets:
+                raise ValueError(
+                    "Inlet mismatch - please ensure correct site is selected."
+                    "Mismatch between inlet in data and inlet in parameters file."
+                )
 
         combined_data = {}
 
