6913b45c354113f80cb542fe7c853360e85bab86
Gareth Jones <oxidising@gmail.com>
2020-12-08 11:23:21 +0000

    WIP: Separating docker build and service files

---
+++ docker/docker_images/openghg_image/build.py
@@ -0,0 +1,18 @@
+import argparse
+import os
+import shutil
+import subprocess
+
+parser = argparse.ArgumentParser(description='Build the base Docker image and optionally push to DockerHub')
+parser.add_argument('tag', metavar='tag', type=str, nargs='+', help='tag name/number')
+parser.add_argument('--push', dest='push', action='store_true', default=False, help='push the image to DockerHub')
+
+args = parser.parse_args()
+
+# We want the latest requirements file for OpenGHG
+shutil.copy2("../../../openghg", "openghg")
+
+# A tag for the image
+tag_str = ":".join(("openghg/openghg-base", str(args.tag[0])))
+
+subprocess.run(["docker", "build", "--tag", tag_str, "."])
---
+++ services/route.py
@@ -0,0 +1,51 @@
+from io import BytesIO
+import json
+from fdk.context import InvokeContext
+from fdk.response import Response
+import logging
+from importlib import import_module
+from typing import Dict, Union
+
+
+def route(ctx: InvokeContext, data: Union[Dict, BytesIO]) -> Response:
+    """ Route the call to a specific function
+
+        Args:
+            ctx: Invoke context. This is passed by Fn to the function
+            data: Data passed to the function by the user
+        Returns:
+            Response: Fn FDK response object containing function call data
+            and data returned from function call
+    """
+    try:   
+        data = json.loads(data.getvalue())
+    except AttributeError:
+        try:
+            data = json.loads(data)
+        except Exception as e:
+            logging.getLogger().info(f"Error parsing JSON argument: {e}")
+            raise ValueError(f"Cannot read function data. Error: {e}")
+
+    function_name = data["function"]
+    args = data["args"]
+
+    # Here we import the module and function from the hugs_service module
+    # Need to add openghg_service. to the start
+    base_module_name = "openghg_service"
+    module_name = ".".join((base_module_name, function_name))
+
+    # Here we import the module and function from the openghg_service module
+    # Need to add openghg_service. to the start
+    try:
+        module = import_module(module_name)
+        fn_to_call = getattr(module, function_name)
+    except (ModuleNotFoundError, AttributeError) as e:
+        raise AttributeError(f"Error calling function {function_name}. Error: {e}")
+
+    # Get the data and headers here?
+    # response_data, headers = fn_to_call(arguments=arguments)
+    response_data = fn_to_call(args=args)
+    # See https://stackoverflow.com/a/20509354
+    headers = {"Content-type": "application/json"}
+
+    return Response(ctx=ctx, response_data=response_data, headers=headers)
