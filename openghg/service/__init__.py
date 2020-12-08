5e93043ca55ccd99f5be2bcc7556b4a2e8c8d146
Gareth Jones <oxidising@gmail.com>
2020-12-08 17:51:19 +0000

    Functions being called correctly. Added return of error from function

--- openghg/service/__init__.py
+++
@@ -1,1 +0,0 @@
-from ._hugs_service import HugsService
--- services/route.py
+++ services/route.py
@@ -2,9 +2,9 @@ from io import BytesIO
 import json
 from fdk.context import InvokeContext
 from fdk.response import Response
-import logging
 from importlib import import_module
 from typing import Dict, Union
+import traceback
 
 
 def route(ctx: InvokeContext, data: Union[Dict, BytesIO]) -> Response:
@@ -19,15 +19,17 @@ def route(ctx: InvokeContext, data: Union[Dict, BytesIO]) -> Response:
     """
     try:   
         data = json.loads(data.getvalue())
-    except AttributeError:
+    except Exception:
         try:
             data = json.loads(data)
         except Exception as e:
-            logging.getLogger().info(f"Error parsing JSON argument: {e}")
-            raise ValueError(f"Cannot read function data. Error: {e}")
+            return Response(ctx=ctx, response_data={"error": str(e)})
 
-    function_name = data["function"]
-    args = data["args"]
+    try:
+        function_name = data["function"]
+        args = data["args"]
+    except KeyError as e:
+        return Response(ctx=ctx, response_data={"error": str(e)})
 
     # Here we import the module and function from the hugs_service module
     # Need to add openghg_services. to the start
@@ -39,13 +41,19 @@ def route(ctx: InvokeContext, data: Union[Dict, BytesIO]) -> Response:
     try:
         module = import_module(module_name)
         fn_to_call = getattr(module, function_name)
-    except (ModuleNotFoundError, AttributeError) as e:
-        raise AttributeError(f"Error calling function {function_name}. Error: {e}")
+    except (ModuleNotFoundError, AttributeError):
+        tb = traceback.format_exc()
+        return Response(ctx=ctx, response_data={"error": str(tb)})
 
-    # Get the data and headers here?
-    # response_data, headers = fn_to_call(arguments=arguments)
-    response_data = fn_to_call(args=args)
-    # See https://stackoverflow.com/a/20509354
+    # # Get the data and headers here?
+    # # response_data, headers = fn_to_call(arguments=arguments)
+    try:
+        response_data = fn_to_call(args=args)
+    except Exception:
+        tb = traceback.format_exc()
+        return Response(ctx=ctx, response_data={"error": str(tb)})
+
+    # # See https://stackoverflow.com/a/20509354
     headers = {"Content-type": "application/json"}
 
     return Response(ctx=ctx, response_data=response_data, headers=headers)
