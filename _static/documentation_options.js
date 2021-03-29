6cf77a06ac6f50c47e045faf618fb144d1cfb465
mrghg <mrghg@users.noreply.github.com>
2021-03-29 14:53:19 +0000

    Deploying to gh-pages from  @ 8611937f54603b415d9bd0c68f150f6eec685887 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g4971fc2',
+    VERSION: '0+untagged.1.g8611937',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
