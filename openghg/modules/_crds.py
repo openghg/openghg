5e9eafa8f03518d15d7f8525ca207adf8e9d472c
Gareth Jones <oxidising@gmail.com>
2021-04-16 14:45:26 +0100

    WIP: Adding inlet read

--- openghg/modules/_crds.py
+++ openghg/modules/_crds.py
@@ -31,24 +31,52 @@ class CRDS:
         """Creates a CRDS object holding data stored within Datasources
 
         Args:
-            filepath: Path of file to load
-            site: Name of site
-            source_id: Source's unique ID
+            data_filepath: Path to file
+            site: Three letter site code
+            network: Network name
+            inlet: Inlet height
+            instrument: Instrument name
+            sampling_period: Sampling period e.g. 2 hour: 2H, 2 minute: 2m
+            measurement_type: Measurement type e.g. insitu, flask
         Returns:
             dict: Dictionary of gas data
         """
         from pathlib import Path
         from openghg.processing import assign_attributes
-        from datetime import datetime
-        from pandas import RangeIndex, read_csv, NaT
-        import warnings
-        from openghg.util import compliant_string
 
         if not isinstance(data_filepath, Path):
             data_filepath = Path(data_filepath)
 
         if not site:
             site = data_filepath.stem.split(".")[0]
+
+        if sampling_period is not None:
+            samp_period = sampling_period
+        else:
+            samp_period = self._sampling_period
+
+        # Process the data into separate Datasets
+        gas_data = self.read_data(data_filepath=data_filepath, site=site, network=network)
+        # Ensure the data is CF compliant
+        gas_data = assign_attributes(data=gas_data, site=site, sampling_period=samp_period)
+
+        return gas_data
+
+    def read_data(self, data_filepath: Path, site: str, network: str) -> Dict:
+        """Separates the gases stored in the dataframe in
+        separate dataframes and returns a dictionary of gases
+        with an assigned UUID as gas:UUID and a list of the processed
+        dataframes
+
+        Args:
+            data_filepath (pathlib.Path): Path of datafile
+        Returns:
+            dict: Dictionary containing metadata, data and attributes keys
+        """
+        from datetime import datetime
+        from pandas import RangeIndex, read_csv, NaT
+        import warnings
+        from openghg.util import compliant_string
 
         # At the moment we're using the filename as the source name
         source_name = data_filepath.stem
@@ -142,10 +170,7 @@ class CRDS:
                 "attributes": site_attributes,
             }
 
-        # Ensure the data is CF compliant
-        gas_data = assign_attributes(data=combined_data, site=site, sampling_period=self._sampling_period)
-
-        return gas_data
+        return combined_data
 
     def read_metadata(self, filepath: Path, data: DataFrame) -> Dict:
         """Parse CRDS files and create a metadata dict
