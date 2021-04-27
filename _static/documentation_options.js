87bb45dfd3238810263b1357c6d1f83da154b573
gareth-j <gareth-j@users.noreply.github.com>
2021-04-27 13:44:31 +0000

    Deploying to gh-pages from  @ 16e98a9ae0cbbd91c9094f57a6e6e232fc7f7fbd 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g5459064',
+    VERSION: '0+untagged.1.g16e98a9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
