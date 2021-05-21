82506755c8c3a4581c24c15f4f2eb700dbe8c658
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2021-05-21 14:54:12 +0100

    Fixing linting issues with added whitespace

--- openghg/dataobjects/_obsdata.py
+++ openghg/dataobjects/_obsdata.py
@@ -25,7 +25,7 @@ class ObsData(_BaseData, abc.Mapping):
         specified.
         Included as a compatability layer for legacy format as a dictionary
         containing a Dataset for each site code.
-        
+
         key (str): Site code
         """
         site = self.metadata["site"].lower()            
@@ -33,14 +33,14 @@ class ObsData(_BaseData, abc.Mapping):
             return self.data
         else:
             raise KeyError(f"Site '{key}' does not match to expected site '{site}'")
-        
+
     def __iter__(self):
         """
         Returns site code as the key for the dictionary as would be expected.
         """
         site = self.metadata["site"]
         return iter([site])
-    
+
     def __len__(self):
         """
         Returns number of key values (fixed at 1 at present)
@@ -48,4 +48,3 @@ class ObsData(_BaseData, abc.Mapping):
         # Fixed length as 1 at the moment but may need to update if other key
         # values are added.
         return 1
-
