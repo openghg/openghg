37085b7993ccb9c106d5a4c2d45558afd8c255ed
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2021-08-05 17:15:31 +0100

    Initial rough draft for demo documents to include. Uploading data, acccesing data and a file to be updated which will make sure the object store is populated with the correct files prior to the demo.

---
+++ notebooks/tutorials/demo/populate_object_store.py
@@ -0,0 +1,8 @@
+import os
+os.environ["OPENGHG_PATH"] = "/work/chxmr/objectStore"
+
+
+# TODO: ADD DETAILS HERE FOR FILES WE NEED IN THE OBJECT STORE FOR THE DEMO
+# Should include measurement data, footprints and emissions
+
+
