480f65a149e34c38f472ce38cdebfaf92f63e0d8
Rachel Tunnicliffe <37222223+rt17603@users.noreply.github.com>
2021-11-30 10:24:20 +0000

    Adding in update and test for getting data up to but not including the end datetime. (#179)

--- openghg/retrieve/_access.py
+++ openghg/retrieve/_access.py
@@ -73,8 +73,10 @@ def get_obs_surface(
     if start_date is not None and end_date is not None:
         start_date_tzaware = timestamp_tzaware(start_date)
         end_date_tzaware = timestamp_tzaware(end_date)
+        end_date_tzaware_exclusive = end_date_tzaware - Timedelta(1, unit="nanosecond")  # Deduct 1 ns to make the end day (date) exclusive.
+
         # Slice the data to only cover the dates we're interested in
-        data = data.loc[dict(time=slice(start_date_tzaware, end_date_tzaware))]
+        data = data.sel(time=slice(start_date_tzaware, end_date_tzaware_exclusive))
 
     try:
         start_date_data = timestamp_tzaware(data.time[0].values)
