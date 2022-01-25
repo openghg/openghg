734f0f8302212bd78884aac72478e7a00b6eea9f
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2022-01-25 15:42:34 +0000

    Adding test for add_noaa_obspack()

--- tests/store/test_populate.py
+++ tests/store/test_populate.py
@@ -1,11 +1,32 @@
 from openghg.store import add_noaa_obspack
 from helpers import get_datapath
-import pytest
 
-@pytest.mark.xfail(reason="Select which ObsPack attributes we want - currently have non JSON serialisable data - see #219 and #193")
-def test_read_noaa_obspack():
-    data_directory = get_datapath("ObsPack/data/nc", data_type="NOAA")
-    print("***data_directory***", data_directory)
-    add_noaa_obspack(data_directory)
+def test_read_noaa_obspack_ch4():
+    '''Test object store can be populated automatically when providing a NOAA
+    ObsPack path. This contains:
+     - directory structure: data/nc/
+     - methane files only: "ch4_..."
+     - surface flask files: "..._surface-flask_..."
+     - aircraft pfp file: "..._aircraft-pfp_..." - this should be ignored for now
+    '''
+    data_directory = get_datapath("ObsPack_ch4", data_type="NOAA")
+    out = add_noaa_obspack(data_directory)
 
-    # TODO: Add checks on any output when added
+    processed = out["processed"]
+
+    # Check first file in folder has been processed
+    filename1 = "ch4_esp_surface-flask_2_representative.nc"
+    species1 = "ch4"
+    assert filename1 in processed.keys()
+    processed_1 = processed[filename1]
+    assert species1 in processed_1.keys()
+
+    # Check second file in folder has been processed
+    filename2 = "ch4_spf_surface-flask_1_ccgg_Event.nc"
+    species2 = "ch4"
+    assert filename2 in processed.keys()
+    processed_2 = processed[filename2]
+    assert species2 in processed_2.keys()
+
+
+# TODO: Add test for multi-species / txt data when able to do so.
