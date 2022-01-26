c118392406df44cc022d7e863407a596f5f4a43b
Gareth Jones <oxidising@gmail.com>
2022-01-26 16:53:51 +0000

    Acquire (for now?) removal complete

--- setup.py
+++ setup.py
@@ -9,18 +9,11 @@ files = ["openghg/*"]
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-with open("requirements-dev.txt") as f:
-    dev_requirements = f.read().splitlines()
-
-with open("requirements-doc.txt") as f:
-    doc_requirements = f.read().splitlines()
-
 setuptools.setup(
     # version=versioneer.get_version(),
     # cmdclass=versioneer.get_cmdclass(),
     version="0.5.0",
     install_requires=requirements,
-    extras_require={"dev": dev_requirements, "docs": doc_requirements},
     name="openghg",
     author="Gareth Jones",
     author_email="g.m.jones@bristol.ac.uk",
