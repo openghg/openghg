58eee272ab9ff215d05fa713c9e015ff303e4f57
Gareth Jones <oxidising@gmail.com>
2022-02-15 15:17:09 +0000

    Removed merge conflicts I'd missed

--- tests/services/test_test_service.py
+++ tests/services/test_test_service.py
@@ -10,15 +10,7 @@
 
 #     monkeypatch.setattr(Timestamp, "now", mock_timestamp)
 
-<<<<<<< HEAD
 # @pytest.mark.skip("Marked for removal")
 # def test_test_service(authenticated_user, mock_timestamp):
 #     timestamp = call_test_service()
-=======
-
-@pytest.mark.skip("Marked for removal")
-def test_test_service(authenticated_user, mock_timestamp):
-    timestamp = call_test_service()
->>>>>>> devel
-
 #     assert timestamp == "Function run at 2001-01-01 00:00:00+00:00"
