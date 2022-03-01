54f89522357f5c7b85c5651cb45472cdb413a44a
Gareth Jones <oxidising@gmail.com>
2022-03-01 15:17:00 +0000

    Added final test to ensure raising with mismatched values

---
+++ tests/standardise/meta/test_metadata.py
@@ -0,0 +1,132 @@
+import pytest
+from openghg.standardise.meta import surface_standardise
+
+
+def test_surface_standardise():
+    metadata = {
+        "site": "sum",
+        "network": "NOAA",
+        "measurement_type": "flask",
+        "species": "ch4",
+        "units": "1",
+        "sampling_period": "NOT_SET",
+        "sampling_period_estimate": "10.0",
+        "instrument": "NOT_SET",
+        "data_owner": "Ed Dlugokencky",
+        "station_longitude": -38.422,
+        "station_latitude": 72.5962,
+        "calibration_scale": "WMO-CH4-X2004A",
+        "inlet": "5m",
+        "inlet_height_magl": "5",
+    }
+
+    attrs = {
+        "site_code": "SUM",
+        "site_name": "Summit",
+        "site_country": "Greenland",
+        "site_country_flag": "http://www.esrl.noaa.gov/gmd/webdata/ccgg/ObsPack/images/flags/GRLD0001.GIF",
+        "site_latitude": 72.5962,
+        "site_longitude": -38.422,
+        "site_elevation": 3209.54,
+        "site_elevation_unit": "masl",
+        "site_position_comment": "This is the nominal location of the site. The sampling location at many sites has changed over time, and we report here the most recent nominal location. The actual sampling location for each observation is not necessarily the site location. The sampling locations for each observation are reported in the latitude, longitude, and altitude variables.",
+        "site_utc2lst": -2.0,
+        "site_utc2lst_comment": "Add 'site_utc2lst' hours to convert a time stamp in UTC (Coordinated Universal Time) to LST (Local Standard Time).",
+        "site_url": "http://www.esrl.noaa.gov/gmd/obop/sum/",
+        "site": "sum",
+        "network": "NOAA",
+        "measurement_type": "flask",
+        "species": "ch4",
+        "units": "1",
+        "sampling_period": "NOT_SET",
+        "sampling_period_estimate": "10.0",
+        "instrument": "NOT_SET",
+        "data_owner": "Ed Dlugokencky",
+        "station_longitude": -38.422,
+        "station_latitude": 72.596,
+        "calibration_scale": "WMO-CH4-X2004A",
+        "inlet": "5m",
+        "inlet_height_magl": "5",
+        "conditions_of_use": "Ensure that you contact the data owner at the outset of your project.",
+        "source": "In situ measurements of air",
+        "Conventions": "CF-1.8",
+        "file_created": "2022-03-01 10:58:14.001490+00:00",
+        "processed_by": "OpenGHG_Cloud",
+        "sampling_period_unit": "s",
+        "station_long_name": "Summit, Greenland",
+        "station_height_masl": 3209.5,
+    }
+
+    updated_metadata = surface_standardise(metadata=metadata, attributes=attrs)
+
+    new_meta = {
+        "site": "sum",
+        "network": "NOAA",
+        "measurement_type": "flask",
+        "species": "ch4",
+        "units": "1",
+        "sampling_period": "NOT_SET",
+        "sampling_period_estimate": "10.0",
+        "instrument": "NOT_SET",
+        "data_owner": "Ed Dlugokencky",
+        "station_longitude": -38.422,
+        "station_latitude": 72.5962,
+        "calibration_scale": "WMO-CH4-X2004A",
+        "inlet": "5m",
+        "inlet_height_magl": "5",
+        "station_long_name": "Summit, Greenland",
+        "station_height_masl": 3209.5,
+    }
+
+    assert metadata != updated_metadata
+    assert updated_metadata == new_meta
+
+
+def test_metadata_latlon_tolerance():
+    metadata = {
+        "station_longitude": -38.422,
+        "station_latitude": 72.5962,
+    }
+
+    attrs = {
+        "station_longitude": -38.422,
+        "station_latitude": 72.5962,
+    }
+
+    surface_standardise(metadata, attrs)
+
+    attrs = {
+        "station_longitude": -38.422,
+        "station_latitude": 72.8,
+    }
+
+    with pytest.raises(ValueError):
+        surface_standardise(metadata, attrs)
+
+    attrs = {
+        "station_longitude": 38.422,
+        "station_latitude": -72.8,
+    }
+
+    with pytest.raises(ValueError):
+        surface_standardise(metadata, attrs)
+
+    attrs = {
+        "station_longitude": -38.423,
+        "station_latitude": 72.597,
+    }
+
+    surface_standardise(metadata, attrs)
+
+
+def test_ensure_mismatch_raises():
+    metadata = {
+        "site": "sum",
+        "network": "NOAA",
+        "measurement_type": "flask",
+    }
+
+    attrs = {"site": "sum", "network": "NOAA", "measurement_type": "swallow-carrying-a-flask"}
+
+    with pytest.raises(ValueError):
+        surface_standardise(metadata, attrs)
