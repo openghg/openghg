eeda55ed23b8d746e2c8e5c2af8f9fa269f42c77
gareth-j <8915182+gareth-j@users.noreply.github.com>
2022-03-02 18:33:56 +0000

    Deploying to gh-pages from @ openghg/openghg@08fa62df1a7d270769f0b35f8ba9438e4861148e 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.2.g5579deb.dirty',
+    VERSION: 'v1.0.0-RC0+0.g08fa62d.dirty',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
