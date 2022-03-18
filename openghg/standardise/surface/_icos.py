34400282f0592ffd92b4b0494a8f32be53bf05c0
Gareth Jones <oxidising@gmail.com>
2022-03-18 12:17:55 +0000

    WIP: Adding processing for long header form ICOS data, tests and test data [skip ci]

--- openghg/standardise/surface/_icos.py
+++ openghg/standardise/surface/_icos.py
@@ -1,5 +1,6 @@
 from typing import Dict, Optional, Union
 from pathlib import Path
+from openghg.util import load_json
 
 
 def parse_icos(
@@ -11,9 +12,69 @@ def parse_icos(
     network: str = "ICOS",
     sampling_period: Optional[str] = None,
     measurement_type: Optional[str] = None,
+    header_type: str = "large",
 ) -> Dict:
-    """Parses an ICOS data file and creates a dictionary of xarray Datasets
-    ready for storage in the object store.
+    """Parses an ICOS data file and creates a dictionary containing the Dataset and metadata
+
+    Args:
+        data_filepath: Path to file
+        site: Three letter site code
+        network: Network name
+        inlet: Inlet height
+        instrument: Instrument name
+        sampling_period: Sampling period e.g. 2 hour: 2H, 2 minute: 2m
+        measurement_type: Measurement type e.g. insitu, flask
+        header_type: ICOS data file with large (40 line) header or shorter single line header
+            Options: large, small
+    Returns:
+        dict: Dictionary of gas data
+    """
+    from pathlib import Path
+    from openghg.standardise.meta import assign_attributes
+
+    if not isinstance(data_filepath, Path):
+        data_filepath = Path(data_filepath)
+
+    if header_type == "large":
+        gas_data = _read_data_large_header(
+            data_filepath=data_filepath,
+            species=species,
+            site=site,
+            inlet=inlet,
+            network=network,
+            instrument=instrument,
+            sampling_period=sampling_period,
+            measurement_type=measurement_type,
+        )
+    else:
+        gas_data = _read_data_small_header(
+            data_filepath=data_filepath,
+            species=species,
+            site=site,
+            inlet=inlet,
+            network=network,
+            instrument=instrument,
+            sampling_period=sampling_period,
+            measurement_type=measurement_type,
+        )
+
+    # Ensure the data is CF compliant
+    gas_data = assign_attributes(data=gas_data, site=site, sampling_period=sampling_period)
+
+    return gas_data
+
+
+def _read_data_large_header(
+    data_filepath: Path,
+    species: str,
+    site: str,
+    inlet: str,
+    network: str,
+    instrument: str,
+    sampling_period: Optional[str] = None,
+    measurement_type: Optional[str] = None,
+) -> Dict:
+    """Parses ICOS files with the larger (~40) line header
 
     Args:
         data_filepath: Path to file
@@ -26,30 +87,153 @@ def parse_icos(
     Returns:
         dict: Dictionary of gas data
     """
-    from pathlib import Path
-    from openghg.standardise.meta import assign_attributes
+    from pandas import read_csv, to_datetime
+    from openghg.util import read_header, clean_string
 
-    if not isinstance(data_filepath, Path):
-        data_filepath = Path(data_filepath)
+    species = clean_string(species)
+    site = clean_string(site)
+    inlet = clean_string(inlet)
+    instrument = clean_string(instrument)
+    network = clean_string(network)
+    sampling_period = clean_string(sampling_period)
+    measurement_type = clean_string(measurement_type)
 
-    gas_data = _read_data(
-        data_filepath=data_filepath,
-        species=species,
-        site=site,
-        inlet=inlet,
-        network=network,
-        instrument=instrument,
-        sampling_period=sampling_period,
-        measurement_type=measurement_type,
+    # Read the header and check its length
+    header = read_header(filepath=data_filepath)
+    len_header = len(header)
+
+    dtypes = {
+        "#Site": "string",
+        "SamplingHeight": "string",
+        "DecimalDate": "float",
+        "Stdev": "float",
+        "NbPoints": "int",
+        "Flag": "string",
+        "InstrumentId": "int",
+        "QualityId": "string",
+        "InternalFlag": "string",
+        "AutoDescriptiveFlag": "string",
+        "ManualDescriptiveFlag": "string",
+    }
+
+    df = read_csv(
+        data_filepath,
+        header=len_header - 1,
+        sep=";",
+        parse_dates={"time": [2, 3, 4, 5, 6]},
+        index_col="time",
+        na_values=["-9.990", "-999.990"],
+        dtype=dtypes,
     )
 
-    # Ensure the data is CF compliant
-    gas_data = assign_attributes(data=gas_data, site=site, sampling_period=sampling_period)
+    df.index = to_datetime(df.index, format="%Y %m %d %H %M")
 
-    return gas_data
+    # Lowercase all the column titles
+    df.columns = [str(c).lower() for c in df.columns]
+
+    # Read some metadata before dropping the columns
+    # sampling_height_data = df["samplingheight"][0]
+    site_name_data = df["#site"][0]
+    species_name_data = df.columns[3]
+
+    # Drop the columns we don't want
+    cols_to_keep = [species_name_data, "stdev", "nbpoints", "flag", "instrumentid"]
+    df = df[cols_to_keep]
+
+    # Remove rows with NaNs in the species or stdev columns
+    df = df.dropna(axis="rows", subset=[species_name_data, "stdev"])
+
+    # Drop duplicate indices
+    df = df.loc[~df.index.duplicated(keep="first")]
+
+    # Check if the index is sorted
+    if not df.index.is_monotonic_increasing:
+        df = df.sort_index()
+
+    rename_dict = {
+        "stdev": species + " variability",
+        "nbpoints": species + " number_of_observations",
+    }
+
+    df = df.rename(columns=rename_dict)
+
+    # Convert to xarray Dataset
+    data = df.to_xarray()
+
+    # Read metadata from the filename and cross check to make sure the passed
+    # arguments match
+    split_filename = data_filepath.name.split(".")
+
+    try:
+        site_fname = split_filename[0]
+        species_fname = split_filename[1]
+        file_sampling_period = split_filename[2]
+        instrument_fname = split_filename[3]
+        inlet_height_fname = split_filename[4]
+    except IndexError:
+        raise ValueError(
+            "Unable to read metadata from filename. We expect a filename such as tta.co2.1minute.222m.dat"
+        )
+
+    if site_fname.lower() != site != site_name_data.lower():
+        raise ValueError("Site mismatch between site argument passed and filename")
+
+    if species_name_data.lower() != species != species_fname.lower():
+        raise ValueError("Mismatch in species between data, argument and filename")
+
+    if inlet_height_fname.lower() != inlet:
+        raise ValueError("Mismatch between inlet height passed and in filename")
+
+    if instrument_fname.lower() != instrument:
+        raise ValueError("Mismatch between instrument passed and that in filename")
+
+    if file_sampling_period == "1minute":
+        file_sampling_period = "60"
+    elif file_sampling_period == "hourly":
+        file_sampling_period = "3600"
+
+    if sampling_period is not None:
+        if file_sampling_period != sampling_period:
+            raise ValueError("Mismatch between sampling period read from filename and that passed.")
+    else:
+        sampling_period = file_sampling_period
+
+    metadata = {
+        "site": site,
+        "species": species,
+        "inlet": inlet,
+        "sampling_period": sampling_period,
+        "network": network,
+        "instrument": instrument,
+    }
+
+    if measurement_type is not None:
+        metadata["measurement_type"] = measurement_type
+
+    unit_line = header[22]
+    if "MEASUREMENT UNIT" in unit_line:
+        units = unit_line.split(":")[1].lower().strip()
+        metadata["units"] = units
+
+    scale_line = header[26]
+    if "MEASUREMENT SCALE" in scale_line:
+        calibration_scale = scale_line.split(":")[1].lower().lstrip(" ").replace(" ", "_").strip()
+        metadata["calibration_scale"] = calibration_scale
+
+    attrs = _retrieve_site_attrs(site=site, network=network)
+
+    species_data = {
+        species: {
+            "metadata": metadata,
+            "data": data,
+            "attributes": attrs,
+        }
+    }
+
+    return species_data
 
 
-def _read_data(
+def _read_data_small_header(
     data_filepath: Path,
     species: str,
     site: str,
@@ -59,33 +243,25 @@ def _read_data(
     sampling_period: Optional[str] = None,
     measurement_type: Optional[str] = None,
 ) -> Dict:
-    """Separates the gases stored in the dataframe in
-    separate dataframes and returns a dictionary of gases
-    with an assigned UUID as gas:UUID and a list of the processed
-    dataframes
-
-    TODO - update this to process multiple species here?
+    """Parses ICOS files with a single line header
 
     Args:
-        data_filepath : Path of datafile
-        species: Species to process
+        data_filepath: Path to file
+        site: Three letter site code
+        network: Network name
+        inlet: Inlet height
+        instrument: Instrument name
+        sampling_period: Sampling period e.g. 2 hour: 2H, 2 minute: 2m
+        measurement_type: Measurement type e.g. insitu, flask
     Returns:
-        dict: Dictionary containing attributes, data and metadata keys
+        dict: Dictionary of gas data
     """
     from pandas import read_csv, Timestamp
-    from openghg.util import read_header, clean_string
+    from openghg.util import read_header
 
     # metadata = read_metadata(filepath=data_filepath, data=data, data_type="ICOS")
     header = read_header(filepath=data_filepath)
     n_skip = len(header) - 1
-
-    species = clean_string(species)
-    site = clean_string(site)
-    inlet = clean_string(inlet)
-    instrument = clean_string(instrument)
-    network = clean_string(network)
-    sampling_period = clean_string(sampling_period)
-    measurement_type = clean_string(measurement_type)
 
     def date_parser(year: str, month: str, day: str, hour: str, minute: str) -> Timestamp:
         return Timestamp(year, month, day, hour, minute)
@@ -143,29 +319,26 @@ def _read_data(
 
     data = data.rename(columns=rename_dict)
 
-    # Conver to xarray Dataset
+    # Convert to xarray Dataset
     data = data.to_xarray()
-
-    combined_data = {}
 
     # Read some metadata from the filename
     split_filename = data_filepath.name.split(".")
 
     try:
         site_fname = split_filename[0]
-
-        if site_fname.lower() != site:
-            raise ValueError("Site mismatch between site argument passed and filename")
-
         file_sampling_period = split_filename[2]
         inlet_height = split_filename[3]
-
-        if inlet_height.lower() != inlet:
-            raise ValueError("Mismatch between inlet height passed and in filename")
     except IndexError:
         raise ValueError(
             "Unable to read metadata from filename. We expect a filename such as tta.co2.1minute.222m.dat"
         )
+
+    if site_fname.lower() != site:
+        raise ValueError("Site mismatch between site argument passed and filename")
+
+    if inlet_height.lower() != inlet:
+        raise ValueError("Mismatch between inlet height passed and in filename")
 
     if file_sampling_period == "1minute":
         file_sampling_period = "60"
@@ -190,9 +363,33 @@ def _read_data(
     if measurement_type is not None:
         metadata["measurement_type"] = measurement_type
 
-    combined_data[species] = {
-        "metadata": metadata,
-        "data": data,
+    attrs = _retrieve_site_attrs(site=site, network=network)
+
+    species_data = {
+        species: {
+            "metadata": metadata,
+            "data": data,
+            "attributes": attrs,
+        }
     }
 
-    return combined_data
+    return species_data
+
+
+def _retrieve_site_attrs(site: str, network: str = "ICOS") -> Dict:
+    """Retrieve site attributes from metadata file
+
+    Args:
+        site: Site code
+        network: Network name, defaults to ICOS
+    Returns:
+        dict: Dictionary of site metadata
+    """
+    site_metadata = load_json(filename="acrg_site_info.json")
+
+    site = site.upper()
+    network = network.upper()
+
+    site_attrs = site_metadata[site][network]
+
+    return site_attrs
