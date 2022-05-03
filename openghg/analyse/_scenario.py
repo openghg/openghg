10606d75db9a85c807e47e5bc21629478529b614
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2022-05-03 18:36:30 +0100

    Making sure == HEAD lines etc. are removed

--- openghg/analyse/_scenario.py
+++ openghg/analyse/_scenario.py
@@ -1369,33 +1369,10 @@ class ModelScenario:
                     )
 
             # Ignoring type below -  - problem with xarray patching np.exp to return DataArray rather than ndarray
-<<<<<<< HEAD
             loss_n: Union[DataArray, float] = np.exp(-1 * scenario["mean_age_particles_n"] / lifetime_hrs).rename('loss_n')  # type: ignore
             loss_e: Union[DataArray, float] = np.exp(-1 * scenario["mean_age_particles_e"] / lifetime_hrs).rename('loss_e')  # type: ignore
             loss_s: Union[DataArray, float] = np.exp(-1 * scenario["mean_age_particles_s"] / lifetime_hrs).rename('loss_s')  # type: ignore
             loss_w: Union[DataArray, float] = np.exp(-1 * scenario["mean_age_particles_w"] / lifetime_hrs).rename('loss_w')  # type: ignore
-=======
-            loss_n: Union[DataArray, float] = np.exp(
-                -1 * scenario["mean_age_particles_n"] / lifetime_hrs
-            ).rename(
-                "loss_n"
-            )  # type:ignore
-            loss_e: Union[DataArray, float] = np.exp(
-                -1 * scenario["mean_age_particles_e"] / lifetime_hrs
-            ).rename(
-                "loss_e"
-            )  # type:ignore
-            loss_s: Union[DataArray, float] = np.exp(
-                -1 * scenario["mean_age_particles_s"] / lifetime_hrs
-            ).rename(
-                "loss_s"
-            )  # type:ignore
-            loss_w: Union[DataArray, float] = np.exp(
-                -1 * scenario["mean_age_particles_w"] / lifetime_hrs
-            ).rename(
-                "loss_w"
-            )  # type:ignore
->>>>>>> f3db2f5f2d93e1418b791e23a2713d33994a59bb
 
         else:
 
