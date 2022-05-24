17cf378b34b27bc5555b1ca6cf6ba6f5e3072d62
rt17603 <rt17603@users.noreply.github.com>
2022-05-24 08:59:32 +0000

    Deploying to gh-pages from @ openghg/openghg@83d35ac038fc11b21bc29d7da22ef7fecef3b8f7 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gaf6d69b.dirty',
+    VERSION: '0+untagged.1.g83d35ac.dirty',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
