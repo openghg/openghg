069b5e9f51fb50b7c11c864046e839838700b9dd
rt17603 <rt17603@users.noreply.github.com>
2022-07-06 13:36:18 +0000

    Deploying to gh-pages from @ openghg/openghg@995f9a6651089627dc0a7410dc590c9d068ad1b4 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g480ecb7.dirty',
+    VERSION: '0+untagged.1.g995f9a6.dirty',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -10,5 +10,5 @@ var DOCUMENTATION_OPTIONS = {
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: true,
     SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: true,
+    ENABLE_SEARCH_SHORTCUTS: false,
 };
