062cdbe9229042847b20839b068c5c8965907b63
Gareth Jones <oxidising@gmail.com>
2022-07-28 14:55:55 +0100

    Adding cloud retrieve test file, removing client folder

--- openghg/client/__init__.py
+++
@@ -1,4 +0,0 @@
-# from ._rank_sources import rank_sources
-# from ._retrieve import retrieve_icos, retrieve_ceda
-# from ._search import search, search_surface
-# from ._observations import get_obs_surface
---
+++ tests/retrieve/test_search_cloud.py
@@ -0,0 +1,37 @@
+import pytest
+from helpers import call_function_packager
+from openghg.dataobjects import SearchResults
+from openghg.retrieve import search_surface
+from openghg.util import compress
+
+
+@pytest.fixture(autouse=True)
+def set_env(monkeypatch):
+    monkeypatch.setenv("OPENGHG_HUB", "1")
+
+
+def test_cloud_search_with_results(mocker):
+    sr = SearchResults(results={"bsd_co2": {"keys": {"1": "1", "2": "2", "3": "3"}}})
+    compressed_sr = compress(sr.to_json().encode("utf-8"))
+
+    content = {"found": True, "result": compressed_sr}
+
+    to_return = call_function_packager(status=200, headers={}, content=content)
+
+    mocker.patch("openghg.cloud.call_function", return_value=to_return)
+
+    result = search_surface(species="co2", site="tac")
+
+    assert result.results == {"bsd_co2": {"keys": {"1": "1", "2": "2", "3": "3"}}}
+
+
+def test_cloud_search_no_results(mocker):
+    content = {"found": False, "result": False}
+
+    to_return = call_function_packager(status=200, headers={}, content=content)
+
+    mocker.patch("openghg.cloud.call_function", return_value=to_return)
+
+    result = search_surface(species="co2", site="tac")
+
+    assert not result.results
