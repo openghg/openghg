56cf906102a4c1627d32514a9f95d82d10a5b9f4
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2022-10-11 16:36:14 +0100

    Adding GC data for Bilsdale for tests

---
+++ tests/data/proc_test_data/GC/bilsdale-md.14.precisions.C
@@ -0,0 +1,11 @@
+# Created: 10 Aug 22 07:26 GMT
+# Standard reported (area or height) CONCENTRATION standard deviations with 3-sigma filter
+#
+#             N2O           SF6     
+# Date          s   n         s   n 
+ 140129       nan   0       nan   0 
+ 140130       nan   0   0.12321  14 
+ 141130   0.19846  24   0.02587  24 
+ 141201   0.17810  24   0.03746  24 
+ 141202   0.21961  21   0.05703  25 
+ 141203   0.19627  22   0.03751  24  
