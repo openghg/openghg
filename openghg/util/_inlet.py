7b9070dab0bcb018d16d63999b538564087f9dc6
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2022-10-28 09:44:37 +0100

    Adding _inlet.py util sub-module

---
+++ openghg/util/_inlet.py
@@ -0,0 +1,50 @@
+from typing import Optional
+
+
+def format_inlet(inlet: Optional[str],
+                 special_keywords: Optional[list] = None) -> Optional[str]:
+    """
+    Make sure inlet name conforms to standard of number followed by unit
+    (or is one of the special keywords).
+    If the string just contains numbers, it is assumed this is in metres and
+    units of "m" will be added.
+
+    e.g. "10m" not "10" or "10magl"
+
+    Args:
+        inlet: Height above ground level
+        special_keywords: Specify special keywords inlet could be set to
+            If so do not apply any formatting.
+            If this is not set a special keyword of "multiple" and "column" will still be allowed.
+
+    Returns:
+        str: formatted inlet string / None
+    """
+
+    if inlet is None:
+        return None
+
+    # By default the special keyword is "multiple" for data containing multiple inlets.
+    # This will be included if data is a combined object from the object store.
+    if special_keywords is None:
+        special_keywords = ["multiple", "column"]
+
+    # Check if inlet is set to a special keyword
+    if inlet in special_keywords:
+        return inlet
+
+    # Check if input inlet just contains numbers and no unit
+    # If so assume the units are metres and add this to the end of the string
+    try:
+        float(inlet)
+    except ValueError:
+        pass
+    else:
+        inlet = inlet + "m"
+
+    # Check if inlet ends with "magl" rather than just "m"
+    # If so remove the end "agl" to just include the "m" value
+    if inlet.endswith("magl"):
+        inlet = inlet.rstrip("lga")
+
+    return inlet
---
+++ tests/util/test_inlet.py
@@ -0,0 +1,35 @@
+import pytest
+from openghg.util import format_inlet
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("10m", "10m"),
+        ("12.2m", "12.2m"),
+        ("123", "123m"),
+        ("123.45", "123.45m"),
+        ("10magl", "10m"),
+        ("13.3magl", "13.3m"),
+        ("multiple", "multiple"),
+        ("column", "column"),
+        (None, None),
+    ],
+)
+def test_format_inlet(test_input, expected):
+    """
+    Test format_inlet formats inlet names in the right way including expected
+    special keywords ("multiple") and None.
+    """
+    output = format_inlet(test_input)
+    assert output == expected
+
+
+def test_format_inlet_special():
+    """
+    Test new special keywords can be specified
+    """
+    special_keyword = "special"
+
+    output = format_inlet(special_keyword, special_keywords=[special_keyword])
+    assert output == special_keyword
