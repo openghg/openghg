57a4af05c81bef76d1b198c4a39d25b0e4ae2c19
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2023-01-05 16:31:04 +0000

    Updating format_inlet function to account for units within key name. This is to allow other keywords to be formatted in a similiar way.

--- tests/util/test_inlet.py
+++ tests/util/test_inlet.py
@@ -25,6 +25,29 @@ def test_format_inlet(test_input, expected):
     assert output == expected
 
 
+@pytest.mark.parametrize(
+    "test_input,expected,key_name",
+    [
+        ("10", "10m", None),
+        ("12.1", "12.1m", "inlet"),
+        ("10m", "10m", "inlet"),
+        ("10m", "10", "inlet_m"),
+        ("10m", "10", "inlet_magl"),
+        ("10magl", "10", "inlet_magl"),
+        ("31m", "31", "station_height_masl"),
+        ("31masl", "31", "station_height_masl"),
+    ],
+)
+def test_format_inlet_keyname(test_input, expected, key_name):
+    """
+    Test format_inlet formats inlet names in the right way when a key_name
+    is specified. The function will derive whether a unit needs to be 
+    included or not.
+    """
+    output = format_inlet(test_input, key_name=key_name)
+    assert output == expected
+
+
 def test_format_inlet_special():
     """
     Test new special keywords can be specified
