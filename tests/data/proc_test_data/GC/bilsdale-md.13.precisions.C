f52ed55d5bf758ad9032ec1d527eacf10a739c9a
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2023-02-27 16:47:50 +0000

    Updating BSD GCMD data to include values in place of some nan values

--- tests/data/proc_test_data/GC/bilsdale-md.13.precisions.C
+++ tests/data/proc_test_data/GC/bilsdale-md.13.precisions.C
@@ -3,18 +3,5 @@
 #
 #             N2O           SF6     
 # Date          s   n         s   n 
- 131215       nan   0       nan   0 
- 131216       nan   0       nan   0 
- 131217       nan   0       nan   0 
- 131218       nan   0       nan   0 
- 131219       nan   0       nan   0 
- 131220       nan   0       nan   0 
- 131223       nan   0       nan   0 
- 131224       nan   0       nan   0 
- 131225       nan   0       nan   0 
- 131226       nan   0       nan   0 
- 131227       nan   0       nan   0 
- 131228       nan   0       nan   0 
- 131229       nan   0       nan   0 
- 131230       nan   0       nan   0 
- 131231       nan   0       nan   0 
+ 131215   0.22222   0   0.12321   0 
+ 131231   0.21111   0   0.12322   0 
