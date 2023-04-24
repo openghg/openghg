58b690ec23307fc56a5b8925530d7a008cc6f163
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2023-04-24 12:12:49 +0100

    Adding explicit list of metadata we expect to be floats

--- openghg/standardise/meta/_metadata.py
+++ openghg/standardise/meta/_metadata.py
@@ -33,6 +33,22 @@ def metadata_default_keys() -> List:
     return default_keys
 
 
+def metadata_keys_as_floats() -> List:
+    """
+    Define which keys should be consistently stored as numbers in the metadata
+    (even if they are not numbers within the attributes).
+    """
+
+    values_as_floats = [
+        # "inlet_height_magl",
+        "station_longitude",
+        "station_latitude",
+        "station_height_masl",
+    ]
+
+    return values_as_floats
+
+
 def sync_surface_metadata(
     metadata: Dict,
     attributes: Dict,
@@ -48,9 +64,10 @@ def sync_surface_metadata(
         keys_to_add: Add these keys to the metadata, if not present, based on
         the attribute values. Note: this skips any keys which can't be
         copied from the attribute values.
-        update_mismatch: If case insensitive mismatch is found between an
-            attribute and a metadata value, update the metadata to contain
-            the attribute value. By default this will raise an AttrMismatchError.
+        update_mismatch: If True, if case insensitive mismatch is found between
+            an attribute and a metadata value, update the metadata to contain
+            the attribute value.
+            If False (and by default) this will raise an AttrMismatchError.
     Returns:
         dict: Copy of metadata updated with attributes
     """
@@ -97,6 +114,7 @@ def sync_surface_metadata(
             pass
 
     default_keys_to_add = metadata_default_keys()
+    keys_as_floats = metadata_keys_as_floats()
 
     if keys_to_add is None:
         keys_to_add = default_keys_to_add
@@ -108,5 +126,8 @@ def sync_surface_metadata(
                 meta_copy[key] = attributes[key]
             except KeyError:
                 logger.warning(f"{key} key not in attributes or metadata")
+            else:
+                if key in keys_as_floats:
+                    meta_copy[key] = float(meta_copy[key])
 
     return meta_copy
