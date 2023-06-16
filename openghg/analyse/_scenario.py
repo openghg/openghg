dd9ace58301a6d2f3eb12c11c165f2dc4b86ce74
prasad <sutar.prasad@yahoo.com>
2023-06-16 23:39:15 +0100

    rich progress bar pending in obssurface

--- openghg/analyse/_scenario.py
+++ openghg/analyse/_scenario.py
@@ -378,7 +378,6 @@ class ModelScenario:
 
             footprint_keyword_options = []
             for fp_inlet_option in fp_inlet_options:
-
                 footprint_keywords = {
                     "site": site,
                     "height": fp_inlet_option,
@@ -504,7 +503,6 @@ class ModelScenario:
         # Search for boundary conditions data based on keywords
         # - domain, species, bc_input
         if domain is not None and bc is None:
-
             bc_keywords = {
                 "species": species,
                 "domain": domain,
@@ -556,7 +554,7 @@ class ModelScenario:
         """
         Find the platform for a site, if present.
 
-        This will access the "site_info.json" file from openghg_defs dependency to 
+        This will access the "site_info.json" file from openghg_defs dependency to
         find this information.
         """
         from openghg.util import get_site_info
@@ -1130,7 +1128,7 @@ class ModelScenario:
 
         import dask.array as da  # type: ignore
         from pandas import date_range
-        from tqdm import tqdm
+        from rich.progress import track
 
         # TODO: Need to work out how this fits in with high time resolution method
         # Do we need to flag low resolution to use a different method? natural / anthro for example
@@ -1265,9 +1263,8 @@ class ModelScenario:
         # at each release time we disaggregate the particles backwards over the previous 24hrs
         # The final value then contains the 29-day integrated residual footprints
         logger.info("Calculating modelled timeseries comparison:")
-        iters = tqdm(time_array.values)
+        iters = track(time_array.values)
         for tt, time in enumerate(iters):
-
             # Get correct index for low resolution data based on start and current date
             current = {dd: getattr(np.datetime64(time, "h").astype(object), dd) for dd in ["month", "year"]}
             tt_low = current["month"] - start["month"] + 12 * (current["year"] - start["year"])
@@ -1453,7 +1450,6 @@ class ModelScenario:
             loss_w: Union[DataArray, float] = np.exp(-1 * scenario["mean_age_particles_w"] / lifetime_hrs).rename("loss_w")  # type: ignore
 
         else:
-
             loss_n = 1.0
             loss_e = 1.0
             loss_s = 1.0
