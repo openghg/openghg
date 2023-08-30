a06c0c6fb1adedc756a2808557ab18dfd92f99cd
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2023-08-30 17:33:45 +0100

    Created abstract MetaStore class

--- openghg/store/metastore/_metastore.py
+++ openghg/store/metastore/_metastore.py
@@ -1,17 +1,54 @@
+"""
+This module defines an interface for metastores.
+
+Metastores store metadata and associated uuid's.
+
+"""
 from __future__ import annotations
-from openghg.store.base import BaseStore, get_data_class
+
+from abc import ABC, abstractmethod
+from typing import Protocol, runtime_checkable
+from typing import Any, Generic, TypeVar
+
+from openghg.dataobjects import SearchResults
 
 
-def open_metastore(bucket: str, data_type: str) -> BaseStore:
-    """Metastore factory function.
+T = TypeVar('T', bound='DataObject')
 
-    args:
-        bucket: object store bucket containing metastore
-        data_type: data type of metastore to open
 
-    returns:
-        Child of BaseStore corresponding to given data type, connected
-    to given bucket.
+@runtime_checkable
+class DataObject(Protocol):
+    """Protocol for objects that can be created
+    via a `load` class method that takes a bucket and
+    a uuid as arguments.
     """
-    data_class = get_data_class(data_type)
-    return data_class(bucket=bucket)
+    @classmethod
+    @abstractmethod
+    def load(cls: type[T], bucket: str, uuid: str) -> T:
+        pass
+
+
+class MetaStore(Generic[T]):
+    data_object: type[T]
+
+    def __init__(self, bucket: str) -> None:
+        self.bucket = bucket
+
+    @abstractmethod
+    def search(self, search_terms: dict[str, Any]) -> SearchResults:
+        """Search for data using a dictionary of search terms."""
+        pass
+
+    @abstractmethod
+    def add(self, metadata: dict[str, Any]) -> str:
+        """Add new metadata to the object store.
+
+        Add creates an uuid for the associated data,
+        stores the metadata together with the uuid,
+        and returns the uuid.
+        """
+        pass
+
+    def get(self, uuid: str) -> T:
+        """Get data stored at given uuid."""
+        return self.data_object.load(self.bucket, uuid)
