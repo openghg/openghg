d6bb8cbbd5d53fc723f4b89aa179f972734a1de5
Gareth Jones <oxidising@gmail.com>
2023-09-12 13:36:33 +0100

    Park this for now, change to current_data

--- openghg/util/_versions.py
+++ openghg/util/_versions.py
@@ -13,7 +13,7 @@ import platform
 import struct
 import subprocess
 import sys
-from typing import List, IO, Union, Tuple
+from typing import List, IO, Union, Tuple, Literal
 
 __all__ = ["show_versions", "check_if_need_new_version"]
 
@@ -166,48 +166,51 @@ def show_versions(file: IO = sys.stdout) -> None:
         print(f"{k}: {stat}", file=file)
 
 
-def check_if_need_new_version(if_exists: str = "default", current_version: str = "default") -> bool:
+def check_if_need_new_version(
+    if_exists: Literal["default", "new", "replace"] = "default",
+    current_data: Literal["default", "keep", "ignore"] = "default",
+) -> bool:
     """
-    Check combination of if_exists and current_version keywords to determine
+    Check combination of if_exists and current_data keywords to determine
     whether a new version should be created.
 
     Output related to these parameters:
-        - if_exists="default", current_version=None
+        - if_exists="default", current_data=None
            - new_version=False (default) - If both values are set
              to None, data will only be updated if there is no
              overlapping data. In this case we can safely write
              to the same version with no data conflict.
-        - if_exists="replace"/"new", current_version=None
+        - if_exists="replace"/"new", current_data=None
            - new_version=True - If a scheme has been set for the
-             combination of new and current data and current_version is None,
+             combination of new and current data and current_data is None,
              create a new version.
-        - if_exists="default"/"replace"/"new", current_version=True
-           - new_version=True - If current_version is explicitly set
+        - if_exists="default"/"replace"/"new", current_data=True
+           - new_version=True - If current_data is explicitly set
              to True, create a new version.
-        - if_exists="default"/"replace"/"new", current_version=False
-           - new_version=False - If current_version is explicitly set
+        - if_exists="default"/"replace"/"new", current_data=False
+           - new_version=False - If current_data is explicitly set
              to False, allow previous version to be overwritten.
 
     Args:
         if_exists: How to combine new and current data, if present.
-        current_version: Whether to save current data or replace this.
+        current_data: Whether to save current data or replace this.
     Returns:
         bool: Whether new version should be created
     """
     # Determining whether a new version should be created based on inputs.
-    if if_exists == "default" and current_version == "default":
+    if if_exists == "default" and current_data == "default":
         # Add new (non-overlapping) data on the same version
         new_version = False
-    elif if_exists != "default" and current_version == "default":
+    elif if_exists != "default" and current_data == "default":
         # If data could be modified based on if_exists input
         # default to creating a new version.
         new_version = True
-    elif current_version == "ignore":
-        # Otherwise match new version to the current_version input.
+    elif current_data == "ignore":
+        # Otherwise match new version to the current_data input.
         new_version = False
-    elif current_version == "keep":
+    elif current_data == "keep":
         new_version = True
     else:
-        raise ValueError("Invalid combination of if_exists and current_version. See documentation.")
+        raise ValueError("Invalid combination of if_exists and current_data. See documentation.")
 
     return new_version
