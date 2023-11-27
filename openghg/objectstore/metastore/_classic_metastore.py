d7101360b28c50346002c66bb6a5d6245054a559
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2023-11-27 15:41:17 +0000

    Fixed import statement...

--- openghg/objectstore/metastore/_classic_metastore.py
+++ openghg/objectstore/metastore/_classic_metastore.py
@@ -28,6 +28,7 @@ import json
 from typing import Literal, Optional, TypeVar
 
 import tinydb
+from tinydb.middlewares import Middleware
 
 from openghg.objectstore import exists, get_object, set_object_from_json
 from openghg.objectstore.metastore._metastore import TinyDBMetaStore
@@ -130,7 +131,7 @@ class BucketKeyStorage(tinydb.Storage):
         pass
 
 
-class SafetyCachingMiddleware(tinydb.Middleware):
+class SafetyCachingMiddleware(Middleware):
     """Middleware that caches changes to the database, and writes
     these changes when the database is closed. Changes are only written
     if the underlying file has not changed. (The underlying file is the
