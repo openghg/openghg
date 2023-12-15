408a8ac5be9b3a9e00a9341f129f8e5fff70a4fb
gareth-j <8915182+gareth-j@users.noreply.github.com>
2023-12-15 16:00:25 +0000

    Deploying to gh-pages from @ openghg/openghg@5560cd2b5922b074fdcd044de03b57d8785dd0db 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0+untagged.1.g6d1ea92',
+    VERSION: '0.7.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -7,7 +7,7 @@ const DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: true,
+    NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
