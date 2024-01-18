7d7922fd007d0ddfa20f54fe6787c2dc76bb4d1f
SutarPrasad <vq21425@bristol.ac.uk>
2024-01-18 11:51:06 +0000

    test in progress

--- tests/store/test_obssurface.py
+++ tests/store/test_obssurface.py
@@ -998,3 +998,22 @@ def test_drop_only_correct_nan():
 
     assert len(rgl_co2_data["time"]) == 1
     assert np.isclose(rgl_co2_data.sel(time=time_str2)["mf"].values, 405.30)
+
+def test_passing_optional_parameters():
+    """ Test to check if the optional parameters like `calibration_scale`
+    are supplied to the parser functions. Can be used in future to add the 
+    """
+
+    data_filepath = get_surface_datapath(filename="DECC-picarro_TAC_20120726_co2-54m-20231106.nc", source_format="OPENGHG")
+
+    data_dict = standardise_surface(filepath=data_filepath, 
+                        source_format="OPENGHG", 
+                        site="TAC", 
+                        network="DECC",
+                        calibration_scale="SIO-05",
+                        instrument='NA',
+                        sampling_period="2H",
+                        store="group" )
+
+    print(data_dict)
+    # assert "SIO-05" in search_data.retrieve().data.attrs['calibration_scale']
