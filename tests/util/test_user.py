8aaaa250307352f0feac2b9cfcca42014213ac07
SutarPrasad <vq21425@bristol.ac.uk>
2024-02-12 20:43:42 +0000

    updated the test logic suitable for github runners

--- tests/util/test_user.py
+++ tests/util/test_user.py
@@ -91,19 +91,29 @@ def test_check_config(mocker, caplog, monkeypatch, tmpdir):
     assert " /tmp/mock_store does not exist but will be created." in caplog.text
 
 
-def test_create_config_duplicates(capsys):
+def test_create_config_duplicates(monkeypatch, mocker, tmpdir, capsys):
     """
     Test simulates input values submitted after invoking the create_config method. It verifies if the value error is raised for duplicate store names and store paths.
     """
-    with patch.object(builtins, 'input', side_effect=iter(["n", "y", "Store1", "/path1", "r", "y", "Store1", "/path1", "r", "n"])):
 
+    monkeypatch.setenv("HOME", str(tmpdir))
+    mock_config_path = Path(tmpdir).joinpath("mock_config.conf")
+
+    mocker.patch("openghg.util._user.get_user_config_path", return_value=mock_config_path)
+    mock_uuids = [f"test-uuid-{x}" for x in range(100, 110)]
+    mocker.patch("openghg.util._user.uuid.uuid4", side_effect=mock_uuids)
+
+    create_config(silent=True)
+
+    config = toml.loads(mock_config_path.read_text())
+
+    with patch.object(builtins, 'input', side_effect=iter(["n", "y", "user", config["object_store"]["user"]["path"], "r", "n", "local_store", str("/path1"), "r", "n"])):
         with pytest.raises(ValueError, match="Paths of the following new stores match those ") as exc_info:
             create_config(silent=False)
 
     exception = exc_info.value
     captured = capsys.readouterr()
 
-    assert "Some names match those of existing stores: ['Store1'], please update manually" in str(captured)
-    assert "Store1" in str(exception)
-    assert "/path1" in str(exception)
-    assert "existing store" in str(exception)
+    assert "Some names match those of existing stores: ['user'], please update manually" in str(captured)
+    assert "user" in str(exception)
+    assert config["object_store"]["user"]["path"] in str(exception)
