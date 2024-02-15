1a31420c5850759233a803d9c434c94c9447b691
Ben <qq23840@bristol.ac.uk>
2024-02-15 11:01:06 +0000

    Pared down test file size to ~45kB each and updated tests accordingly (they all pass)

--- tests/standardise/surface/test_gcwerks_nc.py
+++ tests/standardise/surface/test_gcwerks_nc.py
@@ -11,7 +11,7 @@ mpl_logger.setLevel(logging.WARNING)
 
 @pytest.fixture(scope="session")
 def thd_data():
-    thd_path = get_surface_datapath(filename="AGAGE-GCMD_THD_cfc-11_20240208v1.nc", source_format="GC_nc")
+    thd_path = get_surface_datapath(filename="AGAGE-GCMD_THD_cfc-11_20240208v1-test.nc", source_format="GC_nc")
 
     gas_data = parse_gcwerks_nc(data_filepath=thd_path, site="THD", instrument="gcmd", network="agage",)
 
@@ -21,7 +21,7 @@ def thd_data():
 @pytest.fixture(scope="session")
 def cgo_data():
     cgo_data = get_surface_datapath(
-        filename="AGAGE-GCMS-MEDUSA_CGO_hcfc-133a_20240208v1.nc", source_format="GC_nc"
+        filename="AGAGE-GCMS-MEDUSA_CGO_hcfc-133a_20240208v1-test.nc", source_format="GC_nc"
     )
 
     gas_data = parse_gcwerks_nc(data_filepath=cgo_data, site="cgo", instrument="medusa", network="agage",)
@@ -32,8 +32,8 @@ def cgo_data():
 def test_read_file_capegrim(cgo_data):
     parsed_surface_metachecker(data=cgo_data)
 
-    # Expect two labels, corresponding to the two heights found in process_gcwerks_parameters.json
-    expected_keys = ["hcfc133a_10m", "hcfc133a_70m"]
+    # Expect a single label at 70m in this test file, since only one height in the period convered
+    expected_keys = ["hcfc133a_70m"]
 
     sorted_keys = sorted(list(cgo_data.keys()))
 
@@ -41,7 +41,7 @@ def test_read_file_capegrim(cgo_data):
 
 
 def test_read_file_thd():
-    thd_path = get_surface_datapath(filename="AGAGE-GCMD_THD_cfc-11_20240208v1.nc", source_format="GC_nc")
+    thd_path = get_surface_datapath(filename="AGAGE-GCMD_THD_cfc-11_20240208v1-test.nc", source_format="GC_nc")
 
     gas_data = parse_gcwerks_nc(
         data_filepath=thd_path,
@@ -60,10 +60,10 @@ def test_read_file_thd():
     meas_data = gas_data["cfc11_10m"]["data"]
 
     assert meas_data.time[0] == pd.Timestamp("1995-09-30T17:22:00")
-    assert meas_data.time[-1] == pd.Timestamp("2022-12-31T23:07:00")
+    assert meas_data.time[-1] == pd.Timestamp("1995-11-13T21:38:00")
 
     assert meas_data["cfc11"][0].values.item() == 267.0292663574219
-    assert meas_data["cfc11"][-1] == 218.2406005859375
+    assert meas_data["cfc11"][-1].values.item() == 266.9176025390625
 
 
 @pytest.mark.skip_if_no_cfchecker
@@ -74,7 +74,7 @@ def test_gc_thd_cf_compliance(thd_data):
 
 
 def test_read_invalid_instrument_raises():
-    thd_path = get_surface_datapath(filename="AGAGE-GCMD_THD_cfc-11_20240208v1.nc", source_format="GC_nc")
+    thd_path = get_surface_datapath(filename="AGAGE-GCMD_THD_cfc-11_20240208v1-test.nc", source_format="GC_nc")
 
     with pytest.raises(ValueError):
         parse_gcwerks_nc(
@@ -83,7 +83,7 @@ def test_read_invalid_instrument_raises():
 
 
 def test_expected_metadata_thd_ch4():
-    ch4_path = get_surface_datapath(filename="AGAGE-GCMD_THD_ch4_20240208v1.nc", source_format="GC_nc")
+    ch4_path = get_surface_datapath(filename="AGAGE-GCMD_THD_ch4_20240208v1-test.nc", source_format="GC_nc")
 
     data = parse_gcwerks_nc(data_filepath=ch4_path, site="THD", network="agage", instrument="gcmd")
 
