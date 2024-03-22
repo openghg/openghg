edf5180daf166ae6c2a08847764cf38c6f6c3552
prasad <sutar.prasad@yahoo.com>
2024-03-22 11:47:50 +0000

    updated value error for data_vars check

--- openghg/transform/flux/_edgar.py
+++ openghg/transform/flux/_edgar.py
@@ -287,9 +287,8 @@ def parse_edgar(
 
     # just in case we didn't get the name right...
     if name not in (dvs := edgar_ds.data_vars):
-        name = dvs[0]
-        if version.startswith("v8") and name == "emissions":
-            raise ValueError("We only support 'flx_nc' files, not 'emi_nc' files, for EDGAR v8.")
+        if version.startswith("v8") and name not in dvs:
+            raise ValueError(f"Data variable {name} not present. We only support 'flx_nc' files, not 'emi_nc' files, for EDGAR v8.0")
 
 
     # Convert from kg/m2/s to mol/m2/s
