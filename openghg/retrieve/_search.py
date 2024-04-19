b14a5bb57bf34056c6035b3109ea4c21829834d7
prasad <sutar.prasad@yahoo.com>
2024-04-19 10:35:03 +0100

    chnaged values to false instead of None for search

--- openghg/retrieve/_search.py
+++ openghg/retrieve/_search.py
@@ -109,8 +109,8 @@ def search_flux(
     model: Optional[str] = None,
     start_date: Optional[str] = None,
     end_date: Optional[str] = None,
-    time_resolved: bool = None,
-    high_time_resolution: bool = None,  # DEPRECATED: use time_resolved instead
+    time_resolved: bool = False,
+    high_time_resolution: bool = False,  # DEPRECATED: use time_resolved instead
     period: Optional[Union[str, tuple]] = None,
     continuous: Optional[bool] = None,
     **kwargs: Any,
