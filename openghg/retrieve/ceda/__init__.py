5be9bd00a73be20506bab62268cb59a01240072f
SutarPrasad <vq21425@bristol.ac.uk>
2024-05-10 14:58:12 +0100

    until decision on standardise

--- openghg/retrieve/ceda/__init__.py
+++ openghg/retrieve/ceda/__init__.py
@@ -1,1 +1,2 @@
 from ._retrieve import retrieve_surface
+from ._retrieve import retrieve_column
--- openghg/retrieve/ceda/_retrieve.py
+++ openghg/retrieve/ceda/_retrieve.py
@@ -51,6 +51,53 @@ def retrieve_surface(
         force_retrieval=force_retrieval,
         additional_metadata=additional_metadata,
         store=store,
+        data_type="surface",
+    )
+
+
+def retrieve_column(
+    site: Optional[str] = None,
+    species: Optional[str] = None,
+    inlet: Optional[str] = None,
+    url: Optional[str] = None,
+    force_retrieval: bool = False,
+    additional_metadata: Optional[Dict] = None,
+    store: Optional[str] = None,
+) -> Union[List[ObsData], ObsData, None]:
+    """Retrieve column measurements from the CEDA archive. This function will route the call
+    to either local or cloud functions based on the environment.
+
+    Args:
+        site: Site name
+        species: Species name
+        inlet: Inlet height
+        url: URL of data in CEDA archive
+        force_retrieval: Force the retrieval of data from a URL
+        additional_metadata: Additional metadata to pass if the returned data
+        doesn't contain everythging we need. At the moment we try and find site and inlet
+        keys if they aren't found in the dataset's attributes.
+        For example:
+            {"site": "AAA", "inlet": "10m"}
+        store: Name of object store to use
+    Returns:
+        ObsData or None: ObsData if data found / retrieved successfully.
+
+    Example:
+        To retrieve new data from the CEDA archive using a URL
+        >>> retrieve_column(url=https://dap.ceda.ac.uk/neodc/...)
+        To retrieve already cached data from the object store
+        >>> retrieve_column(site="BSD", species="ch4)
+
+    """
+    return retrieve(
+        site=site,
+        species=species,
+        inlet=inlet,
+        url=url,
+        force_retrieval=force_retrieval,
+        additional_metadata=additional_metadata,
+        store=store,
+        data_type="column",
     )
 
 
@@ -111,10 +158,10 @@ def retrieve(**kwargs: Any) -> Union[List[ObsData], ObsData, None]:
         else:
             return obs_data
     else:
-        return local_retrieve_surface(**kwargs)
+        return local_retrieve(**kwargs)
 
 
-def local_retrieve_surface(
+def local_retrieve(
     site: Optional[str] = None,
     species: Optional[str] = None,
     inlet: Optional[str] = None,
@@ -126,7 +173,7 @@ def local_retrieve_surface(
 ) -> Union[List[ObsData], ObsData, None]:
     """Retrieve surface observations data from the CEDA archive. You can pass
     search terms and the object store will be searched. To retrieve data from the
-    CEDA Archive please browse the website (https://data.ceda.ac.uk/badc) to find
+    CEDA Archive please browse the website for surface (https://data.ceda.ac.uk/badc) and website for column (https://data.ceda.ac.uk/neodc)  to find
     the URL of the dataset to retrieve.
 
     Args:
@@ -153,14 +200,21 @@ def local_retrieve_surface(
     import io
 
     import xarray as xr
-    from openghg.retrieve import search_surface
-    from openghg.store import ObsSurface
+    from openghg.retrieve import search
+    from openghg.store import ObsSurface, ObsColumn
     from openghg.util import download_data, parse_url_filename, site_code_finder, timestamp_now
 
     if additional_metadata is None:
         additional_metadata = {}
 
-    results = search_surface(site=site, species=species, inlet=inlet, data_source="ceda_archive", store=store)
+    results = search(
+        site=site,
+        species=species,
+        inlet=inlet,
+        data_source="ceda_archive",
+        data_type=kwargs["data_type"],
+        store=store,
+    )
 
     if results and not force_retrieval or url is None:
         return results.retrieve_all()
@@ -192,7 +246,7 @@ def local_retrieve_surface(
     # We'll take the attributes as metadata
     metadata = dataset.attrs.copy()
 
-    metadata["data_type"] = "surface"
+    metadata["data_type"] = kwargs["data_type"]
     metadata["data_source"] = "ceda_archive"
     # TODO - how should we find these? Need to change how we're retrieving Datasources
     # using metadata
@@ -201,36 +255,44 @@ def local_retrieve_surface(
 
     # If we're going to be using site, species and inlet here we should check that that
     # information is in the metadata
-    if not {"site", "inlet"} <= metadata.keys():
-        site_name = metadata["station_long_name"]
-        site_code = site_code_finder(site_name=site_name)
+    if kwargs["data_type"] != "column":
+        if not {"site", "inlet"} <= metadata.keys():
+            site_name = metadata["long_name"]
+            site_code = site_code_finder(site_name=site_name)
 
-        if site_code is not None:
-            metadata["site"] = site_code
-        else:
-            if additional_metadata:
-                try:
-                    metadata["site"] = additional_metadata["site"]
-                except KeyError:
-                    logger.error("Unable to read site from additional_metadata.")
-                    return None
+            if site_code is not None:
+                metadata["site"] = site_code
             else:
-                logger.error("Error: cannot find site code, please pass additional metadata.")
-                return None
+                if additional_metadata:
+                    try:
+                        metadata["site"] = additional_metadata["site"]
+                    except KeyError:
+                        logger.error("Unable to read site from additional_metadata.")
+                        return None
+                else:
+                    logger.error("Error: cannot find site code, please pass additional metadata.")
+                    return None
 
-        try:
-            metadata["inlet"] = f"{int(metadata['inlet_height_magl'])}m"
-        except KeyError:
             try:
-                metadata["inlet"] = additional_metadata["inlet"]
+                metadata["inlet"] = f"{int(metadata['inlet_height_magl'])}m"
             except KeyError:
-                logger.error("Unable to read inlet from data or additional_metadata.")
-                return None
+                try:
+                    metadata["inlet"] = additional_metadata["inlet"]
+                except KeyError:
+                    logger.error("Unable to read inlet from data or additional_metadata.")
+                    return None
 
     to_store = {key: {"data": dataset, "metadata": metadata}}
 
     bucket = get_writable_bucket(name=store)
-    with ObsSurface(bucket=bucket) as obs:
-        obs.store_data(data=to_store)
 
-    return ObsData(data=dataset, metadata=metadata)
+    if kwargs["data_type"] == "column":
+        with ObsColumn(bucket=bucket) as obs:
+            obs.store_data(data=to_store)
+
+        return ObsData(data=dataset, metadata=metadata)
+    else:
+        with ObsSurface(bucket=bucket) as obs:
+            obs.store_data(data=to_store)
+
+        return ObsData(data=dataset, metadata=metadata)
