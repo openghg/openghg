bfee61eda0951598f5e004f56a95282dabc12eb7
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2024-06-10 15:45:28 +0100

    Added Metadatum, Metadata classes and tests

---
+++ openghg/store/base/_metadata.py
@@ -0,0 +1,221 @@
+from __future__ import annotations
+from collections import UserDict
+from functools import partial
+from dataclasses import dataclass, field, replace
+from typing import Any, Iterable, Literal, Mapping, Optional, Union
+
+from openghg.objectstore import get_metakeys
+
+
+# TODO: subclasses for different types of data + subclass registry
+# TODO: should "categorising" and "optional" be separate? (e.g. added by some mixin?)
+@dataclass
+class Metadatum:
+    """A single piece of metadata.
+
+    It consists of a name-value pair, as well as two flags:
+    - 'categorizing': if True, this Metadatum will be used for datasource look up
+    - 'required': if True, then 'value' must not be None.
+    """
+
+    name: str
+    value: Any
+    categorising: bool = field(default=False)
+    required: bool = field(default=False)
+
+    def __post_init__(self):
+        """The 'required' attribute can only be True for 'categorising' Metadatum."""
+        if self.categorising is False:
+            self.required = False
+
+    @property
+    def valid(self) -> bool:
+        """Return False if required is True value is None."""
+        invalid = self.required and (self.value is None)
+        return not invalid
+
+    def update_value(self, other: Any) -> Metadatum:
+        if isinstance(other, Metadatum):
+            if other.name != self.name:
+                raise ValueError("Cannot update Metadatum via Metadatum with different name.")
+            new_value = other.value
+        else:
+            new_value = other
+
+        return replace(self, value=new_value)
+
+
+class Metadata(UserDict):
+    """Container for multiple Metadatum objects.
+
+    A Metadata container behaves like a dictionary of name -> value pairs, but behind the
+    scenes, the "value" is actually a Metadatum object, which allows us to control formatting
+    and selecting values.
+
+    For instance,
+
+        >>> metadata = Metadata({"key1": Metadatum(name="key1", value="val1", categorising=True)})
+        >>> metadata["key1"] == "val1"
+        True
+
+    so a Metadata object can be used as a drop-in replacement for a dictionary.
+
+    Similarly,
+
+        >>> list(metadata.values()) == ["val1"]
+        True
+
+    and
+
+        >>> metadata["key1"] == "val2"
+        True
+
+    have the same effect as if `metadata` was a normal dictionary.
+    """
+
+    def __getitem__(self, key: str) -> Any:
+        """Return value of Metadatum with given key.
+
+        Note: this returns the value stored by the Metadatum object with name = key.
+        It does not return the underlying Metadatum object, so that it can be used as an
+        extension of a standard dict.
+        """
+        return super().__getitem__(key).value
+
+    def __setitem__(self, key: str, item: Any) -> None:
+        """Set value stored at `key` to `item`.
+
+        If `key` is already stored, then the value stored at `key` is updated via the Metadatum `update_value`
+        method.
+
+        Otherwise, if `item` is Metadatum, that value is stored at `key` (provided `key` is the same as `item.name`), and
+        otherwise, `item` is a stored as a Metadatum object with value = `item` and name = `key`.
+
+        Args:
+            key: key to store item under
+            item: value or Metadatum object to store (or use to update an existing item)
+
+        Returns:
+            None
+        """
+        if key in self.data:
+            metadatum = self.data[key].update_value(item)  # will raise ValueError if names differ
+        elif isinstance(item, Metadatum):
+            if key != item.name:
+                raise ValueError(f"Cannot store Metadatum with name {item.name} under key {key}; key and name must match.")
+            metadatum = item
+        else:
+            metadatum = Metadatum(name=key, value=item)
+
+        super().__setitem__(key, metadatum)
+
+    @classmethod
+    def from_list(cls, _list: Iterable[Metadatum]) -> Metadata:
+        """Initialise Metadata from a list (or iterable) of Metadatum objects."""
+        metadata = cls()
+        for x in _list:
+            if not isinstance(x, Metadatum):
+                raise ValueError(
+                    f"All items passed to Metadata.from_list must be Metadatum objects; item {x} is {type(x)}."
+                )
+            metadata[x.name] = x
+
+        return metadata
+
+    def insert(self, item: Metadatum) -> None:
+        """Insert Metadatum object into Metadata."""
+        if item.name in self.data:
+            raise ValueError(f"Metadatum '{item.name}' already in Metadata")
+        self.data[item.name] = item
+
+    def select(self, what: Literal["keys", "values", "items"] = "items", **conditions) -> list:
+        """Get keys, values, or times filtered by given conditions.
+
+        Conditions are given as pairs (attr, value), where 'attr' is the name of the attribute to check,
+        and 'value' is the value that the attribute should match.
+
+        If multiple conditions are passed, the items returned will satisfy all of the conditions.
+
+        Args:
+            what: what to return, either keys, values, or items.
+            **conditions: conditions of the form `attribute=value`
+
+        Returns:
+            list of: (key, value) pairs if what == "items", keys if what == "keys", values if what == "values"
+        """
+        def _apply_condition(item: Any, attr: str, value: Any) -> bool:
+            try:
+                retrieved_value = item.__getattribute__(attr)
+            except AttributeError:
+                return False
+            else:
+                return retrieved_value == value
+
+        condition_fns = [partial(_apply_condition, attr=k, value=v) for k, v in conditions.items()]
+
+        filtered_items = [(k, v.value) for k, v in self.data.items() if all(condition(v) for condition in condition_fns)]
+
+        if what == "items":
+            return filtered_items
+        elif what == "keys":
+            return [k for k, _ in filtered_items]
+        else:
+            return [v for _, v in filtered_items]
+
+
+
+def metadata_from_config(bucket: str, data_type: str) -> Metadata:
+    """Make Metadata object from config file.
+
+    Values will be filled with defaults or None.
+
+    Args:
+        bucket: name of object store
+        data_type: type of data (e.g. "footprints", "surface", "flux", etc.)
+
+    Returns:
+        Metadata object contains required and optional keys from config file.
+    """
+    try:
+        metakeys = get_metakeys(bucket=bucket)[data_type]
+    except KeyError:
+        raise ValueError(f"No metakeys for {data_type}, please update metakeys configuration file.")
+
+    metadata = []
+
+    for name, spec in metakeys["required"].items():
+        # TODO: use type to look up appropriate subclass of Metadatum
+        value = spec.get("default", None)
+        metadata.append(Metadatum(name=name, value=value, categorising=True, required=True))
+
+    for name, spec in metakeys.get("optional", {}).items():
+        # TODO: use type to look up appropriate subclass of Metadatum
+        value = spec.get("default", None)
+        metadata.append(Metadatum(name=name, value=value, categorising=True, required=False))
+
+    return Metadata.from_list(metadata)
+
+
+def categorising_keys_valid(metadata: Metadata) -> bool:
+    """Return True if all categorising items are valid."""
+    n_cat = len(metadata.select(categorising=True))
+    n_cat_valid = len(metadata.select(categorising=True, valid=True))
+    return n_cat == n_cat_valid
+
+
+def get_datasource_lookup_metadata(metadata: Metadata, min_keys: Optional[int] = None) -> dict[str, Any]:
+    """Get metadata needed for datasource look up."""
+    if not categorising_keys_valid(metadata):
+        invalid_keys = metadata.select("keys", categorising=True, valid=False)
+        valid_keys = metadata.select("keys", categorising=True, valid=True)
+
+        if min_keys is None:
+            invalid_keys_str = "\n\t".join(invalid_keys)
+            raise ValueError(f"The following required keys are missing:{invalid_keys_str}")
+
+        elif len(valid_keys) < min_keys:
+            raise ValueError(
+                f"{min_keys} required keys necessary, but only {len(valid_keys)} required keys provided."
+            )
+
+    return {k: v for k, v in metadata.select(categorising=True, valid=True)}  # TODO: add `if v is not None` ?
---
+++ tests/store/test_metadata.py
@@ -0,0 +1,171 @@
+import pytest
+
+from openghg.objectstore import create_default_config, get_metakey_defaults
+from openghg.store.base._metadata import Metadatum, Metadata, categorising_keys_valid, metadata_from_config
+
+
+def test_metadatum_postinit():
+    """Metadatum can only have `require = True` if `categorising = True`"""
+    non_cat_metadatum = Metadatum(name="ncmd", value=None, categorising=False, required=True)
+
+    assert non_cat_metadatum.required is False
+
+
+def test_metadatum_valid_check():
+    """Test `valid` method of Metadatum. It should only be False for Metadatum with
+    `categorising = True` and `value = None`
+     """
+    valid_metadatum1 = Metadatum(name="vmd1", value=None, categorising=True, required=False)
+    assert valid_metadatum1.valid
+
+    valid_metadatum2 = Metadatum(name="vmd2", value="not none", categorising=True, required=True)
+    assert valid_metadatum2.valid
+
+    valid_metadatum3 = Metadatum(name="vmd3", value=None, categorising=False, required=False)
+    assert valid_metadatum3.valid
+
+    invalid_metadatum = Metadatum(name="invmd", value=None, categorising=True, required=True)
+    assert not invalid_metadatum.valid
+
+
+def test_metadatum_update_value():
+    """Test updating the value of a Metadatum object via an explict value
+    or via another Metadatum object. Check that a Metadatum object can only have
+    its value updated by another Metadatum object if they have the same name, and
+    test that other attributes are unchanged."""
+    metadatum1 = Metadatum(name="md1", value=None)
+
+    metadatum2 = metadatum1.update_value("val1")
+
+    assert metadatum2 == Metadatum(name="md1", value="val1")
+
+    metadatum3 = Metadatum(name="md1", value="val1", categorising=True)
+    metadatum4 = metadatum1.update_value(metadatum3)
+
+    assert metadatum4 == Metadatum(name="md1", value="val1")
+
+    with pytest.raises(ValueError):
+        metadatum1.update_value(Metadatum(name="md2", value=None))
+
+
+def test_metadata_init():
+    metadatum1 = Metadatum(name="md1", value=None)
+    metadatum2 = Metadatum(name="md2", value=None)
+    metadatum3 = Metadatum(name="md3", value=None)
+
+    metadata = Metadata.from_list([metadatum1, metadatum2, metadatum3])
+
+    assert ("md1" in metadata)  and ("md2" in metadata) and ("md3" in metadata)
+
+
+def test_metadata_get_set():
+    metadata = Metadata()
+
+    # test adding as with a dictionary
+    metadata["species"] = "CH4"
+
+    # we can retrieve as with a dictionary
+    assert metadata["species"] == "CH4"
+
+    # the .data attribute contains an actual dictionary with the Metadatum
+    # in this case, the default values for 'categorising' and 'required' are used
+    assert metadata.data["species"] == Metadatum(name="species", value="CH4")
+
+
+def test_metadata_update():
+    metadata = Metadata.from_list([Metadatum(name="species", value=None, categorising=True, required=True)])
+
+    # currently not valid
+    assert not metadata.data["species"].valid
+
+    # update value so it is a valid required Metadatum
+    metadata["species"] = "CH4"
+
+    assert metadata.data["species"].valid
+    assert metadata["species"] == "CH4"
+
+
+def test_init_vs_from_list():
+    metadatum1 = Metadatum(name="md1", value=1)
+    metadatum2 = Metadatum(name="md2", value=2)
+    metadatum3 = Metadatum(name="md3", value=3)
+
+    metadata1 = Metadata.from_list([metadatum1, metadatum2, metadatum3])
+
+    _dict = {"md1": 1, "md2": 2, "md3": 3}
+
+    metadata2 = Metadata(_dict)
+
+    assert metadata1 == metadata2
+
+    metadatum_dict = {"md1": metadatum1, "md2": metadatum2, "md3": metadatum3}
+    metadata3 = Metadata(metadatum_dict)
+
+    assert metadata2 == metadata3
+
+
+def test_from_config(tmp_path):
+    create_default_config(bucket=str(tmp_path))
+    flux_metakey_defaults = get_metakey_defaults()["flux"]
+
+    metadata = metadata_from_config(bucket=str(tmp_path), data_type="flux")
+
+    for key in flux_metakey_defaults["required"]:
+        assert key in metadata
+
+        metadatum = metadata.data[key]
+        assert metadatum.categorising and metadatum.required
+
+    for key in flux_metakey_defaults["optional"]:
+        assert key in metadata
+
+        metadatum = metadata.data[key]
+        assert metadatum.categorising and not metadatum.required
+
+
+def test_metadata_processing(tmp_path):
+    """Test processing that combines user metadata """
+    create_default_config(bucket=str(tmp_path))
+
+    config_metadata = metadata_from_config(bucket=str(tmp_path), data_type="flux")
+
+    # set-up user inputs:
+    # "required" inputs
+    # "optonal" inputs (e.g. via kwargs in standardise)
+    # "info" inputs (meant only for searching, not as categorising metadata)
+    user_required = {"species": "ch4", "source": "edgar-annual-total", "domain": "europe"}
+    user_optional = {"database": "edgar", "database_version": "v8.0"}
+    user_info = {"project": "paris"}
+
+    # combine user inputs
+    user_metadata_dict = user_info.copy()
+    user_metadata_dict.update(user_optional)
+    user_metadata_dict.update(user_required)
+
+    user_metadata = Metadata(user_metadata_dict)
+
+    # update the config_metadata, which should fill all of the values
+    # in the config with the user's inputs
+    config_metadata.update(user_metadata)
+
+    # all required values have been provided, so categorising metadata
+    # should be valid
+    assert categorising_keys_valid(config_metadata)
+
+    # check values
+    for k, v in config_metadata.items():
+        # user provided all required and optional keys except "model"
+        if k != "model":
+            assert v == user_metadata[k]
+
+    # "model" should not have been updated
+    assert config_metadata["model"] is None
+
+    # the "info" metadata should not be categorising
+    assert config_metadata.data["project"].categorising is False
+
+    # we can recover the user's combined metadata
+    user_metadata_recovered = dict(config_metadata)
+    del user_metadata_recovered["model"]
+
+    assert user_metadata_recovered == user_metadata
