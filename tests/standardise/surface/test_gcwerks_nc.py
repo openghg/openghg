66d3ee0c6518a5459fe9e31f4814536856759cfb
Ben <qq23840@bristol.ac.uk>
2024-07-03 15:33:45 +0100

    reconfigure tests for new input files

--- tests/standardise/surface/test_gcwerks_nc.py
+++ tests/standardise/surface/test_gcwerks_nc.py
@@ -12,7 +12,7 @@ mpl_logger.setLevel(logging.WARNING)
 @pytest.fixture(scope="session")
 def thd_data():
     thd_path = get_surface_datapath(
-        filename="AGAGE-GCMD_THD_cfc-11_20240208v1-test.nc", source_format="GC_nc"
+        filename="agage_thd_cfc-11_20240513-test.nc", source_format="GC_nc"
     )
 
     gas_data = parse_gcwerks_nc(data_filepath=thd_path, site="THD", instrument="gcmd", network="agage",)
@@ -23,7 +23,7 @@ def thd_data():
 @pytest.fixture(scope="session")
 def cgo_data():
     cgo_data = get_surface_datapath(
-        filename="AGAGE-GCMS-MEDUSA_CGO_hcfc-133a_20240208v1-test.nc", source_format="GC_nc"
+        filename="agage_cgo_hcfc-133a_20240513-test.nc", source_format="GC_nc"
     )
 
     gas_data = parse_gcwerks_nc(data_filepath=cgo_data, site="cgo", instrument="medusa", network="agage",)
@@ -44,7 +44,7 @@ def test_read_file_capegrim(cgo_data):
 
 def test_read_file_thd():
     thd_path = get_surface_datapath(
-        filename="AGAGE-GCMD_THD_cfc-11_20240208v1-test.nc", source_format="GC_nc"
+        filename="agage_thd_cfc-11_20240513-test.nc", source_format="GC_nc"
     )
 
     gas_data = parse_gcwerks_nc(
@@ -57,11 +57,11 @@ def test_read_file_thd():
 
     parsed_surface_metachecker(data=gas_data)
 
-    expected_key = ["cfc11_10m"]
+    expected_key = ["cfc11_15m"]
 
     assert sorted(list(gas_data.keys())) == expected_key
 
-    meas_data = gas_data["cfc11_10m"]["data"]
+    meas_data = gas_data["cfc11_15m"]["data"]
 
     assert meas_data.time[0] == pd.Timestamp("1995-09-30T17:22:00")
     assert meas_data.time[-1] == pd.Timestamp("1995-11-13T21:38:00")
@@ -79,7 +79,7 @@ def test_gc_thd_cf_compliance(thd_data):
 
 def test_read_invalid_instrument_raises():
     thd_path = get_surface_datapath(
-        filename="AGAGE-GCMD_THD_cfc-11_20240208v1-test.nc", source_format="GC_nc"
+        filename="agage_thd_cfc-11_20240513-test.nc", source_format="GC_nc"
     )
 
     with pytest.raises(ValueError):
@@ -88,26 +88,26 @@ def test_read_invalid_instrument_raises():
         )
 
 
-def test_expected_metadata_thd_ch4():
-    ch4_path = get_surface_datapath(filename="AGAGE-GCMD_THD_ch4_20240208v1-test.nc", source_format="GC_nc")
+def test_expected_metadata_thd_cfc11():
+    cfc11_path = get_surface_datapath(filename="agage_thd_cfc-11_20240513-test.nc", source_format="GC_nc")
 
-    data = parse_gcwerks_nc(data_filepath=ch4_path, site="THD", network="agage", instrument="gcmd")
+    data = parse_gcwerks_nc(data_filepath=cfc11_path, site="THD", network="agage", instrument="gcmd")
 
-    metadata = data["ch4_10m"]["metadata"]
+    metadata = data["cfc11_15m"]["metadata"]
 
     expected_metadata = {
         "data_type": "surface",
         "instrument": "gcmd",
         "site": "THD",
         "network": "agage",
-        "sampling_period": "1",
-        "units": "ppb",
-        "calibration_scale": "TU-87",
-        "inlet": "10m",
-        "species": "ch4",
-        "inlet_height_magl": "10",
-        "data_owner": "Ray Weiss",
-        "data_owner_email": "rfweiss@ucsd.edu",
+        "sampling_period": "1.0",
+        "units": "1e-12",
+        "calibration_scale": "SIO-05",
+        "inlet": "15m",
+        "species": "cfc11",
+        "inlet_height_magl": 15.0,
+        "data_owner": "Ray F. Weiss, Jens Muhle",
+        "data_owner_email": "rfweiss@ucsd.edu, jmuhle@ucsd.edu",
         "station_longitude": -124.151,
         "station_latitude": 41.0541,
         "station_height_masl": 107.0,
