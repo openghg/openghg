458e71f26366caca509c6eb27ea73868c9588dc2
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2024-07-15 12:42:26 +0100

    Added debugging print statement

--- openghg/analyse/_scenario.py
+++ openghg/analyse/_scenario.py
@@ -1296,6 +1296,8 @@ class ModelScenario:
                 {"H_back": np.arange(0, max_h_back, highest_res_H)[::-1]}
             )
 
+            print(flux_hf_rolling.H_back)
+
             # select subsequence of H_back times to match high res fp (i.e. fp without max H_back coord)
             flux_hf_rolling = flux_hf_rolling.sel(H_back=fp_high_time_res.H_back)
 
