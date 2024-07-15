09522702df4608b2b40bce864ae44cbbc3d5dcec
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2024-07-15 13:09:36 +0100

    More debug statements

--- openghg/analyse/_scenario.py
+++ openghg/analyse/_scenario.py
@@ -1247,6 +1247,10 @@ class ModelScenario:
             date_start_back.values, date_end.values, freq=highest_resolution, inclusive="left"
         ).to_numpy()
 
+        for k, v in locals().items():
+            if str(k).endswith("_H"):
+                print(k, v)
+
         # Create low frequency flux data (monthly)
         flux_ds_low_freq = flux_ds.resample({"time": "1MS"}).mean().sel(time=slice(date_start_back, date_end))
         flux_ds_low_freq = flux_ds_low_freq.transpose(*("lat", "lon", "time"))
