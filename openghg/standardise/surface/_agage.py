0575cc0981e0b8d1e1847cc1d27251758857ee09
prasad <sutar.prasad@yahoo.com>
2024-07-24 21:42:58 +0100

    if condition for attibute instrument

--- openghg/standardise/surface/_agage.py
+++ openghg/standardise/surface/_agage.py
@@ -205,8 +205,8 @@ def _format_species(
 
         attributes = file_params
         # Need to stop the "instrument" attr from being overwritten in combined files
-        attributes["instrument_name"] = attributes.pop("instrument")
-        attributes["instrument_name"] = metadata["instrument"]
+        if "instrument" in attributes.keys():
+            attributes["instrument_name"] = attributes.pop("instrument")
 
         metadata_keys = metadata_default_keys()
 
