ed916bf3763eeac601041c56b61639e58bbf448d
prasad <sutar.prasad@yahoo.com>
2024-08-13 11:06:04 +0100

    flake8 reformatting

--- openghg/util/_util.py
+++ openghg/util/_util.py
@@ -4,7 +4,7 @@
 
 from collections.abc import Iterable
 from pathlib import Path
-from typing import Any, Dict, Iterator, Optional, Tuple, Union
+from typing import Any, Dict, Iterator, Optional, Tuple
 import logging
 
 from openghg.types import multiPathType
