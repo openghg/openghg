f9ba6210c0e730b20ad146eecd5225f7b5164c24
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2024-10-14 17:22:29 +0100

    fixup! Renamed `Store` to `VersionedStore`

--- openghg/store/storage/__init__.py
+++ openghg/store/storage/__init__.py
@@ -1,5 +1,5 @@
 from ._chunking import ChunkingSchema
-from .versioned_store import VersionedStore
+from ._versioned_store import VersionedStore
 from ._localzarrstore import LocalZarrStore
 from ._encoding import get_zarr_encoding
 from ._compression import compare_compression
