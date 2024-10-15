4ee4ec344b5c0764a3f6d37fc2035ed6869bc300
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2024-10-15 12:40:59 +0100

    Expore Store, MemoryStore

--- openghg/store/storage/__init__.py
+++ openghg/store/storage/__init__.py
@@ -1,5 +1,6 @@
 from ._chunking import ChunkingSchema
 from ._versioned_store import VersionedStore
+from ._store import Store, MemoryStore
 from ._localzarrstore import LocalZarrStore
 from ._encoding import get_zarr_encoding
 from ._compression import compare_compression
