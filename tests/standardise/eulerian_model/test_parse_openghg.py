fc018bfe468c2d271b3fe2a797aaa588d7701cad
prasad <sutar.prasad@yahoo.com>
2024-11-24 16:14:01 +0000

    updated test_parse_openghg in eulerian_model

--- tests/standardise/eulerian_model/test_parse_openghg.py
+++ tests/standardise/eulerian_model/test_parse_openghg.py
@@ -2,10 +2,16 @@ from helpers import get_eulerian_datapath
 from openghg.standardise.eulerian_model import parse_openghg
 
 
-def test_read_file():
+def test_parse_openghg():
     """ This tests the parser for Eulerian model
     Looks for processed key and metadata associated with the file"""
 
     test_datapath = get_eulerian_datapath("GEOSChem.SpeciesConc.20150101_0000z_reduced.nc4")
 
     proc_results = parse_openghg(filepath=test_datapath, model="GEOSChem", species="ch4")
+
+    assert "geoschem_ch4_2015-01-01" in proc_results
+
+    metadata = proc_results["geoschem_ch4_2015-01-01"]["metadata"]
+    assert "eulerian_model" in metadata["data_type"]
+    assert "2015-01-01 00:00:00z" in metadata["simulation_start_date_and_time"]
