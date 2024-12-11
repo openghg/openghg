2f2d2308c0ecdfb2faf6ab261beabfd6cfabe2a4
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2024-12-11 17:35:25 +0000

    Including define_obs_types as a top level function which can be accessed directly through datapack

--- openghg/datapack/__init__.py
+++ openghg/datapack/__init__.py
@@ -1,1 +1,7 @@
-from ._obspack import retrieve_data, create_obspack, define_obspack_filename, define_obspack_name
+from ._obspack import (
+    define_obs_types,
+    retrieve_data,
+    create_obspack,
+    define_obspack_filename,
+    define_obspack_name,
+)
