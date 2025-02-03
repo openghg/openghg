ad566f28710277ea2a27c59e6a4b83bc16d88aa2
SutarPrasad <vq21425@bristol.ac.uk>
2025-02-03 09:03:36 +0000

    removed type hints

--- openghg/standardise/_standardise.py
+++ openghg/standardise/_standardise.py
@@ -208,43 +208,43 @@ def standardise_column(
     """Read column observation file
 
     Args:
-        filepath (str | Path): Path to the input observation file (CSV, HDF5, etc.).
-        species (str): Species name or synonym (e.g., "ch4").
-        platform (str, optional): Type of platform (default is "satellite"). Can be one of:
+        filepath: Path to the input observation file (CSV, HDF5, etc.).
+        species: Species name or synonym (e.g., "ch4").
+        platform: Type of platform (default is "satellite"). Can be one of:
             - "satellite"
             - "site"
-        region (str, optional): The geographic region covered by the data ("BRAZIL", "INDIA", "UK").
-        site (str, optional): Site name or code, if platform is site-based.
-        satellite (str, optional): Satellite name (if platform is satellite-based).
-        domain (str, optional): For satellite data, specifies the geographical domain identifier.
+        region: The geographic region covered by the data ("BRAZIL", "INDIA", "UK").
+        site: Site name or code, if platform is site-based.
+        satellite: Satellite name (if platform is satellite-based).
+        domain: For satellite data, specifies the geographical domain identifier.
             This can either map to an existing domain or define a new domain.
-        selection (str, optional): A unique identifier for a data selection (e.g., "land", "glint").
+        selection: A unique identifier for a data selection (e.g., "land", "glint").
             If not specified, `domain` is used.
-        network (str, optional): Name of the satellite or in-situ measurement network (e.g., "TCCON").
-        instrument (str, optional): Instrument name used to collect data (e.g., "TANSO-FTS").
-        source_format (str, optional): Format of the input data (default is "openghg").
-        store (str, optional): The name of the store to write the processed data to.
-        if_exists (str, optional): Determines behavior if data already exists in the store.
+        network: Name of the satellite or in-situ measurement network (e.g., "TCCON").
+        instrument: Instrument name used to collect data (e.g., "TANSO-FTS").
+        source_format: Format of the input data (default is "openghg").
+        store: The name of the store to write the processed data to.
+        if_exists: Determines behavior if data already exists in the store.
             Can be one of:
             - "auto" (default): Checks for overlap and decides whether to add or raise an error.
             - "new": Only includes new data, ignoring existing data.
             - "combine": Replaces and inserts new data into the existing timeseries.
-        save_current (str, optional): Decides whether to save the current version of the data:
+        save_current: Decides whether to save the current version of the data:
             - "auto" (default): Automatically saves based on `if_exists` behavior.
             - "yes" or "y": Save current data as a separate version.
             - "no" or "n": Allow updates or deletion of current data.
-        overwrite (bool, optional): Deprecated. Replaced by `if_exists="new"`.
-        force (bool, optional): Forces the addition of data even if it's identical to the existing data.
-        compression (bool, optional): Enables or disables compression during data storage (default is True).
-        compressor (Any, optional): Custom compression method. Defaults to `Blosc(cname="zstd", clevel=5, shuffle=Blosc.SHUFFLE)`.
+        overwrite: Deprecated. Replaced by `if_exists="new"`.
+        force: Forces the addition of data even if it's identical to the existing data.
+        compression: Enables or disables compression during data storage (default is True).
+        compressor: Custom compression method. Defaults to `Blosc(cname="zstd", clevel=5, shuffle=Blosc.SHUFFLE)`.
             See https://zarr.readthedocs.io/en/stable/api/codecs.html for more information on compressors.)`.
-        filters (Any, optional): Filters to apply during data storage. Defaults to no filtering.
+        filters: Filters to apply during data storage. Defaults to no filtering.
         https://zarr.readthedocs.io/en/stable/tutorial.html#filters for more information on picking filters.
-        chunks (dict, optional): Specifies chunking schema for data storage (default is None). It expects a dictionary of dimension name and chunk size,
+        chunks: Specifies chunking schema for data storage (default is None). It expects a dictionary of dimension name and chunk size,
             for example {"time": 100}. If None then a chunking schema will be set automatically by OpenGHG.
             See documentation for guidance on chunking: https://docs.openghg.org/tutorials/local/Adding_data/Adding_ancillary_data.html#chunking
             To disable chunking pass an empty dictionary.
-        optional_metadata (dict, optional): Additional metadata to tag the data (e.g., `{"project": "paris"}`).
+        optional_metadata: Additional metadata to tag the data (e.g., `{"project": "paris"}`).
 
     Returns:
         dict: Dictionary containing confirmation of standardisation process.
