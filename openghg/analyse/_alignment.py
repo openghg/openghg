b6a24596027eae977aa7bf13b0425f4bb41b7d42
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2025-03-06 14:18:31 +0000

    Extracted functions from align_obs_and_other
    Added tests for these parts

--- openghg/analyse/_alignment.py
+++ openghg/analyse/_alignment.py
@@ -3,12 +3,101 @@ from typing import cast, Literal
 
 import numpy as np
 import pandas as pd
+from numpy.typing import ArrayLike
 
-from openghg.types import XrDataLikeMatch, XrDataLikeMatch2
+from openghg.types import XrDataLike, XrDataLikeMatch, XrDataLikeMatch2
 
 
 logger = logging.getLogger("openghg.analyse")
 logger.setLevel(logging.INFO)  # Have to set level for logger as well as handler
+
+
+def extract_obs_freq(obs: XrDataLike) -> float | None:
+    obs_attributes = obs.attrs
+
+    if "averaged_period" in obs_attributes:
+        return float(obs_attributes["averaged_period"])
+
+    if "sampling_period" in obs_attributes:
+        sampling_period = obs_attributes["sampling_period"]
+
+        if sampling_period == "NOT_SET":
+            return None
+
+        if sampling_period == "multiple":
+            # If we have a varying sampling_period, make sure we always resample to footprint
+            return 1.0
+
+        return float(sampling_period)
+
+    if "sampling_period_estimate" in obs_attributes:
+        estimate = obs_attributes["sampling_period_estimate"]
+        logger.warning(f"Using estimated sampling period of {estimate}s for observational data")
+        return float(estimate)
+
+    return None
+
+
+def infer_freq_in_seconds(times: ArrayLike, tol: float = 1.0) -> float:
+    obs_data_period_s = np.nanmedian(np.diff(times) / 1e9).astype("float32")
+
+    obs_data_period_s_min = np.diff(times).min() / 1e9
+    obs_data_period_s_max = np.diff(times).max() / 1e9
+
+    max_diff = (obs_data_period_s_max - obs_data_period_s_min).astype(float)
+
+    # Check if the periods differ by more than 1 second
+    if max_diff > tol:
+        raise ValueError("Sample period can be not be derived from observations")
+
+    return float(obs_data_period_s)
+
+
+def time_overlap(
+    times1: np.ndarray, times2: np.ndarray, freq1: pd.Timedelta, freq2: pd.Timedelta
+) -> tuple[pd.Timestamp, pd.Timestamp]:
+    """Find start and end dates where gives times overlap.
+
+    Frequencies for each time array are used to pad the end dates.
+
+    Args:
+        times1: array of times
+        times2: array of times
+        freq1: frequency/period corresponding to times1
+        freq2: frequency/period corresponding to times2
+
+    Returns:
+        tuple of start and end dates
+
+    """
+
+    startdate1 = pd.to_datetime(times1[0])
+    startdate2 = pd.to_datetime(times2[0])
+
+    # add frequencies to end dates
+    enddate1 = pd.to_datetime(times1[-1]) + freq1
+    enddate2 = pd.to_datetime(times2[-1]) + freq2
+
+    start_date = max(startdate1, startdate2)
+    end_date = min(enddate1, enddate2)
+
+    return start_date, end_date
+
+
+def tweak_start_and_end_dates(
+    start_date: pd.Timestamp, end_date: pd.Timestamp, freq2: pd.Timedelta
+) -> tuple[pd.Timestamp, pd.Timestamp, pd.Timestamp]:
+    # Ensure lower range is covered for times1
+    start1 = start_date - pd.Timedelta("1ns")
+
+    # Ensure extra buffer is added for footprint based on fp timeperiod.
+    # This is to ensure footprint can be forward-filled to obs (in later steps)
+    start2 = start_date - (freq2 - pd.Timedelta("1ns"))
+
+    # Subtract very small time increment (1 nanosecond) to make this an exclusive selection
+    end = end_date - pd.Timedelta("1ns")
+
+    return start1, start2, end
 
 
 def align_obs_and_other(
@@ -17,21 +106,27 @@ def align_obs_and_other(
     resample_to: Literal["obs", "other", "coarsest"] | str = "coarsest",
     platform: str | None = None,
 ) -> tuple[XrDataLikeMatch, XrDataLikeMatch2]:
-    """
-    Slice and resample obs and footprint data to align along time
-    This slices the date to the smallest time frame
-    spanned by both the footprint and obs, using the sliced start date
+    """Slice and resample obs and footprint data to align along time.
+
+    This slices the date to the smallest time frame spanned by both the footprint and obs,
+    using the sliced start date.
+
     The time dimension is resampled based on the resample_to input using the mean.
     The resample_to options are:
      - "coarsest" - resample to the coarsest resolution between obs and footprints
      - "obs" - resample to observation data frequency
      - "footprint" - resample to footprint data frequency
      - a valid resample period e.g. "2H"
+
     Args:
+        obs: obs data (either as xr.DataArray or xr.Dataset)
+        other: other data to align with (either as xr.DataArray or xr.Dataset)
         resample_to: Resample option to use: either data based or using a valid pandas resample period.
         platform: Observation platform used to decide whether to resample
+
     Returns:
-        tuple: Two xarray DataArrays with aligned time dimensions
+        tuple: Two xarray DataArrays or Datasets with aligned time dimensions. The types of the returned
+          data match the types of the input data.
     """
     resample_keyword_choices = ("obs", "other", "coarsest")
 
@@ -47,41 +142,12 @@ def align_obs_and_other(
         if platform == "satellite":
             return obs, other
 
-    # Whether sampling period is present or we need to try to infer this
-    infer_sampling_period = False
-    # Get the period of measurements in time
-    obs_attributes = obs.attrs
-    if "averaged_period" in obs_attributes:
-        obs_data_period_s = float(obs_attributes["averaged_period"])
-    elif "sampling_period" in obs_attributes:
-        sampling_period = obs_attributes["sampling_period"]
-        if sampling_period == "NOT_SET":
-            infer_sampling_period = True
-        elif sampling_period == "multiple":
-            # If we have a varying sampling_period, make sure we always resample to footprint
-            obs_data_period_s = 1.0
-        else:
-            obs_data_period_s = float(sampling_period)
-    elif "sampling_period_estimate" in obs_attributes:
-        estimate = obs_attributes["sampling_period_estimate"]
-        logger.warning(f"Using estimated sampling period of {estimate}s for observational data")
-        obs_data_period_s = float(estimate)
-    else:
-        infer_sampling_period = True
+    # try to get period/freq from attributes
+    obs_data_period_s = extract_obs_freq(obs)
 
-    if infer_sampling_period:
-        # Attempt to derive sampling period from frequency of data
-        obs_data_period_s = np.nanmedian((obs.time.data[1:] - obs.time.data[0:-1]) / 1e9).astype("float32")
-
-        obs_data_period_s_min = np.diff(obs.time.data).min() / 1e9
-        obs_data_period_s_max = np.diff(obs.time.data).max() / 1e9
-
-        max_diff = (obs_data_period_s_max - obs_data_period_s_min).astype(float)
-
-        # Check if the periods differ by more than 1 second
-        if max_diff > 1.0:
-            raise ValueError("Sample period can be not be derived from observations")
-
+    # if None is returned, we need to try to infer the period/frequency
+    if obs_data_period_s is None:
+        obs_data_period_s = infer_freq_in_seconds(obs.time.values)
         estimate = f"{obs_data_period_s:.1f}"
         logger.warning(f"Sampling period was estimated (inferred) from data frequency: {estimate}s")
         obs.attrs["sampling_period_estimate"] = estimate
@@ -89,7 +155,7 @@ def align_obs_and_other(
     # TODO: Check regularity of the data - will need this to decide is resampling
     # is appropriate or need to do checks on a per time point basis
 
-    obs_data_period_ns = obs_data_period_s * 1e9
+    obs_data_period_ns = int(obs_data_period_s * 1e9)
     obs_data_timeperiod = pd.Timedelta(obs_data_period_ns, unit="ns")
 
     # Derive the footprints period from the frequency of the data
@@ -103,29 +169,22 @@ def align_obs_and_other(
         elif obs_data_timeperiod < other_data_timeperiod:
             resample_to = "footprint"
 
-    # Here we want timezone naive pd.Timestamps
-    # Add sampling period to end date to make sure resample includes these values when matching
-    obs_startdate = pd.Timestamp(obs.time[0].values)
-    obs_enddate = pd.Timestamp(obs.time[-1].values) + obs_data_timeperiod
-    footprint_startdate = pd.Timestamp(other.time[0].values)
-    footprint_enddate = pd.Timestamp(other.time[-1].values) + other_data_timeperiod
+    # get common start and end dates, accounting for periods in the end dates
+    start_date, end_date = time_overlap(
+        obs.time.values, other.time.values, obs_data_timeperiod, other_data_timeperiod
+    )
 
-    start_date = max(obs_startdate, footprint_startdate)
-    end_date = min(obs_enddate, footprint_enddate)
-
-    # Ensure lower range is covered for obs
-    start_obs_slice = start_date - pd.Timedelta("1ns")
-    # Ensure extra buffer is added for footprint based on fp timeperiod.
-    # This is to ensure footprint can be forward-filled to obs (in later steps)
-    start_other_slice = start_date - (other_data_timeperiod - pd.Timedelta("1ns"))
-    # Subtract very small time increment (1 nanosecond) to make this an exclusive selection
-    end_slice = end_date - pd.Timedelta("1ns")
+    # tweak to make end date exclusive, obs start inclusive, and other start time nearly one full period earlier
+    start_obs_slice, start_other_slice, end_slice = tweak_start_and_end_dates(
+        start_date, end_date, other_data_timeperiod
+    )
 
     obs = obs.sel(time=slice(start_obs_slice, end_slice))
     other = other.sel(time=slice(start_other_slice, end_slice))
 
     if obs.time.size == 0 or other.time.size == 0:
         raise ValueError("Obs data and Footprint data don't overlap")
+
     # Only non satellite datasets with different periods need to be resampled
     timeperiod_diff_s = np.abs(obs_data_timeperiod - other_data_timeperiod).total_seconds()
     tolerance = 1e-9  # seconds
---
+++ tests/analyse/test_alignment.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from openghg.analyse._alignment import infer_freq_in_seconds, time_overlap, tweak_start_and_end_dates
+from openghg.retrieve import get_obs_surface, get_footprint
+
+
+@pytest.fixture
+def obs_data():
+    start_date = "2012-01-01"
+    end_date = "2013-01-01"
+
+    site = "tac"
+    species = "ch4"
+    network = "DECC"
+    inlet = "100m"
+
+    obs_surface = get_obs_surface(
+        site=site, species=species, start_date=start_date, end_date=end_date, inlet=inlet, network=network
+    )
+    return obs_surface
+
+
+@pytest.fixture
+def footprint_data():
+    start_date = "2012-01-01"
+    end_date = "2013-01-01"
+
+    site = "tac"
+    domain = "EUROPE"
+    inlet = "100m"
+
+    footprint = get_footprint(
+        site=site, domain=domain, height=inlet, start_date=start_date, end_date=end_date
+    )
+    return footprint
+
+
+def test_infer_freq_in_seconds():
+    times = pd.date_range("2020-01-01", freq="3s", periods=10).values
+
+    assert 3 == infer_freq_in_seconds(times)
+
+    times[3] += pd.Timedelta("0.5s")
+
+    assert 3 == infer_freq_in_seconds(times)
+
+
+def test_infer_freq_in_seconds_tolerance():
+    """Test that ValueError is raised if diff between max/min gaps is above tolerance.
+
+    Also test that we can raise tolerance.
+    """
+    times = pd.date_range("2020-01-01", freq="3s", periods=10).values
+    times[3] += pd.Timedelta("1s")
+
+    with pytest.raises(ValueError):
+        infer_freq_in_seconds(times)
+
+    assert 3 == infer_freq_in_seconds(times, tol=2.0)
+
+
+def test_infer_freq_footprint():
+    times = pd.date_range("2020-01-01", freq="1h", periods=10).values
+    times[3] += pd.Timedelta("1m")
+
+    inferred_freq = pd.to_timedelta(np.nanmedian(np.diff(times)))
+
+    assert inferred_freq == pd.to_timedelta(1, unit="h")
+
+
+def test_time_overlap():
+    input_start1 = "2020-01-01 01:20:00"
+    input_end = "2020-01-01 12:00:00"
+
+    freq1 = pd.to_timedelta("20min")
+    times1 = pd.date_range(input_start1, input_end, freq=freq1)
+
+    input_start2 = "2020-01-01 02:00:00"
+    freq2 = pd.to_timedelta("1h")
+    times2 = pd.date_range(input_start2, input_end, freq=freq2)
+
+    start_date, end_date = time_overlap(times1.values, times2.values, freq1, freq2)
+    start1, start2, end = tweak_start_and_end_dates(start_date, end_date, freq2)
+
+    expected_start1 = pd.to_datetime(input_start2) - pd.to_timedelta(1)
+    assert start1 == expected_start1
+
+    expected_start2 = pd.to_datetime(input_start2) - freq2 + pd.to_timedelta(1)
+    assert start2 == expected_start2
+
+    # freq1 is shorter, so this will give the earliest end date (since times1 and times2 have same end date)
+    expected_end = pd.to_datetime(input_end) - pd.to_timedelta(1) + freq1
+    assert end == expected_end
