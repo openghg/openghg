754a716b1079f7ed2406862d7b46693898a8a117
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2025-04-01 09:52:36 +0100

    Updating measurement_type to link to platform when this is not explicitly specified.

--- openghg/store/_obssurface.py
+++ openghg/store/_obssurface.py
@@ -161,7 +161,10 @@ class ObsSurface(BaseStore):
             dataset_source: Dataset source name, for example "ICOS", "InGOS", "European ObsPack", "CEDA 2023.06"
             sampling_period: Sampling period in pandas style (e.g. 2H for 2 hour period, 2m for 2 minute period).
             platform: Type of measurement platform e.g. "surface-insitu", "surface-flask"
-            measurement_type: Type of measurement e.g. insitu, flask
+            measurement_type: Type of measurement. For some source_formats this value is added
+                to the attributes. Platform should be used in preference.
+                If platform is specified and measurement_type is not, this will be
+                set to match the platform.
             verify_site_code: Verify the site code
             site_filepath: Alternative site info file (see openghg/openghg_defs repository for format).
                 Otherwise will use the data stored within openghg_defs/data/site_info JSON file by default.
@@ -245,6 +248,9 @@ class ObsSurface(BaseStore):
         instrument = clean_string(instrument)
 
         sampling_period = evaluate_sampling_period(sampling_period)
+
+        if measurement_type is None and platform is not None:
+            measurement_type = platform
 
         # Ensure we have a clear missing value for data_level, data_sublevel
         data_level = format_data_level(data_level)
