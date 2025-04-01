36fef3744b01a4fa1392d23d0cba28b1cfcde24a
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2025-04-01 13:46:38 +0100

    Adding new _platform.py util module and including 'get_platform_from_info' function

--- openghg/util/__init__.py
+++ openghg/util/__init__.py
@@ -47,6 +47,7 @@ from ._metadata_util import (
     get_overlap_keys,
     merge_dict,
 )
+from ._platform import get_platform_from_info
 from ._registry import AutoRegistry, Registry
 from ._site import get_site_info, sites_in_network
 from ._species import (
---
+++ openghg/util/_platform.py
@@ -0,0 +1,27 @@
+from openghg.util._site import get_site_info
+from openghg.types import optionalPathType
+
+
+def get_platform_from_info(site: str, site_filepath: optionalPathType = None) -> str | None:
+    """Find the platform for a site, if present.
+
+    This will access the "site_info.json" file from openghg_defs dependency to
+    find this information.
+
+    Args:
+        site: Site code (usually 3-letters).
+    Returns:
+        str | None: platform name from site_info.json if present
+    """
+    
+    site_data = get_site_info(site_filepath=site_filepath)
+
+    site_upper = site.upper()
+
+    try:
+        site_details = site_data[site_upper]
+    except KeyError:
+        return None
+    else:
+        platform: str = site_details.get("platform")
+        return platform
