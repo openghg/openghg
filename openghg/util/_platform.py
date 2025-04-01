b49082fb6ee933bc39bea601aa13ae607a35745d
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2025-04-01 17:53:57 +0100

    Adding initial platform checking functions (not tested yet)

--- openghg/util/_platform.py
+++ openghg/util/_platform.py
@@ -1,5 +1,105 @@
+from typing import overload
+import logging
 from openghg.util._site import get_site_info
-from openghg.types import optionalPathType
+from openghg.types import optionalPathType, MetadataFormatError
+
+logger = logging.getLogger("openghg.util")
+logger.setLevel(logging.DEBUG)  # Have to set level for logger as well as handler
+
+
+def define_platform(data_type: str | list | None = None) -> list:
+    """
+    Define platform values which can be used. These describe the type of data and
+    platform used to measure the data.
+
+    Args:
+        data_type: Specify the data_type to select the associated platforms.
+            For "surface" this is currently:
+                - "surface-insitu"
+                - "surface-flask"
+            For "column" this is currently:
+                - "satellite"
+                - "column-insitu"
+                - "column"
+            For "mobile" this is currently:
+                - "aircraft-flask"
+                - "balloon-flask"
+                - "mobile-flask"
+            If no data_type is specified, this will return all platform values.
+    Returns:
+        list: Platform values
+    """
+
+    platform_values = []
+
+    platform_values_all = {
+        "surface": [
+            "surface-insitu",
+            "surface-flask",
+        ],
+        "column": [
+            "satellite",
+            "column-insitu",
+            "column",
+        ],
+        "mobile": [
+            "aircraft-flask",
+            "balloon-flask",
+            "mobile-flask",
+        ],
+    }
+
+    obs_types = ["surface", "column", "mobile"]
+
+    if data_type is None:
+        data_types = obs_types
+    elif isinstance(data_type, str):
+        data_types = [data_type]
+    else:
+        data_types = data_types
+
+    for data_type in data_types:
+        platform_values.extend(platform_values_all[data_type])
+
+    return platform_values
+
+
+@overload
+def verify_platform(
+    platform: str,
+) -> str: ...
+
+
+@overload
+def verify_platform(
+    platform: None,
+) -> None: ...
+
+
+def verify_platform(platform: str | None, data_type: str | None = None) -> str | None:
+    """
+    Check platform is a suitable value based on the values within define_platform.
+
+    Args:
+        platform: Platform name to check
+        data_type: Type of data associated with the platform.
+            This is generally associated with observation measurements.
+            These are defined within openghg.store.spec.define_data_types function.
+    Returns:
+        str | None: checked platform value
+    """
+
+    platform_values = define_platform(data_type=data_type)
+
+    if platform is not None:
+        if platform.lower() not in platform_values:
+            msg = f"Platform currently set to '{platform}'. This must be one of: {platform_values}"
+            logger.exception(msg)
+            raise MetadataFormatError(msg)
+        else:
+            return platform
+    else:
+        return platform
 
 
 def get_platform_from_info(site: str, site_filepath: optionalPathType = None) -> str | None:
