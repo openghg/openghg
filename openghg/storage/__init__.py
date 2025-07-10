26f1213f1690d01c9aade08f9c9d6866e6f101b4
Brendan Murphy <brendan.murphy@bristol.ac.uk>
2025-07-10 13:21:36 +0100

    Added ZarrStore class
    Parametrized tests to run with implementations of new ZarrStore class,
    as well as on MemoryStore.
    
    MemoryStore was changed to perform updates that match ZarrStore.

--- openghg/storage/__init__.py
+++ openghg/storage/__init__.py
@@ -1,1 +1,4 @@
 from ._store import Store, MemoryStore
+from ._zarr_store import get_zarr_directory_store, get_zarr_memory_store
+
+__all__ = ("MemoryStore", "Store", "get_zarr_directory_store", "get_zarr_memory_store")
--- tests/storage/test_store.py
+++ tests/storage/test_store.py
@@ -4,6 +4,7 @@ import pytest
 import xarray as xr
 
 from openghg.storage import MemoryStore
+from openghg.storage._zarr_store import get_zarr_directory_store, get_zarr_memory_store
 from openghg.types import DataOverlapError
 
 
@@ -26,86 +27,173 @@ ds3 = dummy_dataset(idx3)
 ds4 = dummy_dataset(idx4)
 ds5 = dummy_dataset(idx5)
 
+# test data for updating
+twice_ds1 = dummy_dataset(idx1).map(lambda x: 2 * x)
+twice_ds2 = dummy_dataset(idx2).map(lambda x: 2 * x)  # non-contiguous with idx1
+
 
 @pytest.fixture()
-def ms():
+def memory_store():
     """Test MemoryStore."""
     return MemoryStore()
 
 
-def test_insert_creates(ms):
-    assert not ms
-
-    ms.insert(ds1)
-
-    assert ms
-
-    xr.testing.assert_equal(ms.get(), ds1)
+@pytest.fixture()
+def zarr_memory_store():
+    """Test ZarrStore with zarr.MemoryStore as underlying storage."""
+    return get_zarr_memory_store()
 
 
-def test_clear(ms):
-    assert not ms
-
-    ms.insert(ds1)
-
-    assert ms
-
-    ms.clear()
-
-    assert not ms
-    xr.testing.assert_identical(ms.get(), xr.Dataset())
+@pytest.fixture()
+def zarr_directory_store(tmp_path):
+    """Test ZarrStore with zarr.DirectoryStore as underlying storage."""
+    return get_zarr_directory_store(path=tmp_path)
 
 
-def test_insert_twice(ms):
-    ms.insert(ds1)
-    ms.insert(ds5)
+# names of fixtures to use in parametrize
+store_names = ["memory_store", "zarr_memory_store", "zarr_directory_store"]
+
+
+# To use fixtures in parametrize, use the "request" fixture, as detailed here:
+# https://stackoverflow.com/questions/42014484/pytest-using-fixtures-as-arguments-in-parametrize
+@pytest.mark.parametrize("store_name", store_names)
+def test_insert_creates(store_name, request):
+    store = request.getfixturevalue(store_name)
+
+    assert not store
+
+    store.insert(ds1)
+
+    assert store
+
+    xr.testing.assert_equal(store.get(), ds1)
+
+
+@pytest.mark.parametrize("store_name", store_names)
+def test_clear(store_name, request):
+    store = request.getfixturevalue(store_name)
+
+    assert not store
+
+    store.insert(ds1)
+
+    assert store
+
+    store.clear()
+
+    assert not store
+    xr.testing.assert_identical(store.get(), xr.Dataset())
+
+
+@pytest.mark.parametrize("store_name", store_names)
+def test_insert_twice(store_name, request):
+    store = request.getfixturevalue(store_name)
+
+    store.insert(ds1)
+    store.insert(ds5)
 
     expected = np.hstack([ds1.x.values, ds5.x.values])
 
-    np.testing.assert_equal(ms.get().x.values, expected)
+    np.testing.assert_equal(store.get().x.values, expected)
 
 
-def test_error_on_insert_conflict(ms):
-    ms.insert(ds1)
+@pytest.mark.parametrize("store_name", store_names)
+def test_error_on_insert_conflict(store_name, request):
+    """Test an error is raised on conflict."""
+    store = request.getfixturevalue(store_name)
+
+    store.insert(ds1)
 
     with pytest.raises(DataOverlapError):
-        ms.insert(ds1)
+        store.insert(ds1)
 
 
-def test_insert_ignore_conflict(ms):
-    ms.insert(ds1)
+@pytest.mark.parametrize("store_name", store_names)
+def test_insert_ignore_conflict(store_name, request):
+    """Test that insert with `on_conflict = 'ignore'` inserts non-conflicting values."""
+    store = request.getfixturevalue(store_name)
+
+    store.insert(ds1)
 
     # all values should be ignored
-    ms.insert(ds1, on_conflict="ignore")
+    store.insert(ds1, on_conflict="ignore")
 
     # ds4 has four non-conflicting values, so four values should be added
-    ms.insert(ds4, on_conflict="ignore")
+    store.insert(ds4, on_conflict="ignore")
 
     # check values
     expected = np.hstack([ds1.x.values, ds4.x.values[-4:]])
 
-    np.testing.assert_equal(ms.get().x.values, expected)
+    np.testing.assert_equal(store.get().x.values, expected)
 
 
-def test_update(ms):
-    ms.insert(ds1)
+@pytest.mark.parametrize("store_name", store_names)
+def test_update(store_name, request):
+    store = request.getfixturevalue(store_name)
 
-    xr.testing.assert_equal(ms.get(), ds1)
+    store.insert(ds1)
+
+    xr.testing.assert_equal(store.get(), ds1)
 
     # update the values
-    twice_ds1 = ds1.map(lambda x: 2 * x)
-    ms.update(twice_ds1)
+    store.update(twice_ds1)
 
-    xr.testing.assert_equal(ms.get(), twice_ds1)
+    xr.testing.assert_equal(store.get(), twice_ds1)
 
 
-def test_update_ignore_nonconflicts(ms):
-    ms.insert(ds1)
+@pytest.mark.parametrize("store_name", store_names)
+def test_update_ignore_nonconflicts(store_name, request):
+    """Test `update` with non-conflicts ignored."""
+    store = request.getfixturevalue(store_name)
 
-    xr.testing.assert_equal(ms.get(), ds1)
+    store.insert(ds1)
+
+    xr.testing.assert_equal(store.get(), ds1)
 
     # update the values with "ignore"
-    ms.update(ds4, on_nonconflict="ignore")
+    store.update(ds4, on_nonconflict="ignore")
 
     expected = np.hstack([ds1.x.values[:4], ds4.x.values[:-4]])
-    np.testing.assert_equal(ms.get().x.values, expected)
+    np.testing.assert_equal(store.get().x.values, expected)
+
+
+@pytest.mark.parametrize("store_name, is_zarr", [(name, "zarr" in name) for name in store_names])
+def test_non_contiguous_update(store_name, is_zarr, request):
+    """Test `update` when only some of the values are updated.
+
+    This raises an error with Zarr stores because the region to update is non-contiguous.
+    We will fix this later.
+    """
+    store = request.getfixturevalue(store_name)
+
+    store.insert(ds1)
+
+    xr.testing.assert_equal(store.get(), ds1)
+
+    # update the values in a non-contiguous region
+    if not is_zarr:
+        store.update(twice_ds2)
+
+        expected = np.hstack([twice_ds2.x.values[:5], ds1.x.values[5:10], twice_ds2.x.values[5:]])
+
+        np.testing.assert_equal(store.get().x.values, expected)
+    else:
+        with pytest.raises(NotImplementedError):
+            store.update(twice_ds2)
+
+
+@pytest.mark.parametrize("store_name", store_names)
+def test_contiguous_update(store_name, request):
+    """Test `update` on contiguous region of times."""
+    store = request.getfixturevalue(store_name)
+
+    store.insert(ds1)
+
+    # insert data starting after data in ds1
+    store.insert(ds5)
+
+    # update contiguous region (same region as ds1)
+    store.update(twice_ds1)
+
+    expected = np.hstack([twice_ds1.x.values, ds5.x.values])
+    np.testing.assert_equal(store.get().x.values, expected)
