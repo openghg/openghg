bc9c353eb63d237e4b163220044d0bbd0acaf2b5
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2025-07-11 11:57:57 +0100

    Adding initial read_file function to BaseStore with associated parameters and _standardise_from_file method

--- openghg/store/base/_base.py
+++ openghg/store/base/_base.py
@@ -97,8 +97,367 @@ class BaseStore:
     ) -> list[dict] | None:
         raise NotImplementedError
 
-    def read_file(self, *args: Any, **kwargs: Any) -> list[dict]:
-        raise NotImplementedError
+    def _standardise_from_file(
+        self,
+        filepath: str | Path | list[str | Path],
+        fn_input_parameters: dict,
+        source_format: str | None = None,
+        parser_fn: callable | None = None,
+        update_mismatch: str = "never",
+        if_exists: str = "auto",
+        new_version: bool = True,
+        compressor: Any | None = None,
+        filters: Any | None = None,
+        chunks: dict | None = None,
+        info_metadata: dict | None = None,
+        additional_metadata: dict | None = None,
+    ) -> list[dict]:
+        """
+        Standardise input data from a filepath or set of filepaths. This will also
+        store the data in the object store.
+
+        Args:
+            filepath: Filepath or filepaths to data to be standardised.
+            fn_input_parameters: Set of input parameters from read_file
+            source_format: Name of associated format for the provide filepath. This
+                will access an appropriate parse function to use for standardisation.
+            parser_fn: Option to pass parser function directly rather than a source_format.
+            update_mismatch: This determines how mismatches between the internal data
+                "attributes" and the supplied / derived "metadata" are handled.
+                This includes the options:
+                    - "never" - don't update mismatches and raise an AttrMismatchError
+                    - "from_source" / "attributes" - update mismatches based on input data (e.g. data attributes)
+                    - "from_definition" / "metadata" - update mismatches based on associated data (e.g. site_info.json)
+                **Note: at the moment this has only been implemented for ObsSurface**
+            if_exists: What to do if existing data is present.
+                - "new" - just include new data and ignore previous
+                - "combine" - replace and insert new data into current timeseries
+            new_version: Whether to create a new version of the datasource.
+            compressor: A custom compressor to use. If None, this will default to
+                `Blosc(cname="zstd", clevel=5, shuffle=Blosc.SHUFFLE)`.
+                See https://zarr.readthedocs.io/en/stable/api/codecs.html for more information on compressors.
+            filters: Filters to apply to the data on storage, this defaults to no filtering. See
+                https://zarr.readthedocs.io/en/stable/tutorial.html#filters for more information on picking filters
+            chunks: Chunking schema to use when storing data. It expects a dictionary of dimension name and chunk size,
+                for example {"time": 100}. If None then a chunking schema will be set automatically by OpenGHG.
+                See documentation for guidance on chunking: https://docs.openghg.org/tutorials/local/Adding_data/Adding_ancillary_data.html#chunking.
+                To disable chunking pass in an empty dictionary.
+            info_metadata: Allows to pass in additional tags to describe the data. e.g {"comment":"Quality checks have been applied"}
+            additional_metadata: Additional metadata to include for the datasource.
+        Returns:
+            list[dict]: List of datasources and their uuids
+
+        TODO: Consider how to apply update_mismatch (via align_metadata_attributes() methods)
+            for all data types, rather than just ObsSurface.
+        TODO: Apply check_chunks for all data types (currently just Footprint) but make sure
+            this still works as expected.
+        """
+
+        from openghg.util import load_standardise_parser, split_function_inputs
+
+        chunking_params = self.find_chunking_schema_inputs()
+        validate_params = self.find_data_schema_inputs()
+
+        if not parser_fn:
+            # Load the data retrieve object
+            parser_fn = load_standardise_parser(data_type=self._data_type, source_format=source_format)
+
+        # Define parameters to pass to the parser function and remaining keys
+        parser_input_parameters, additional_input_parameters = split_function_inputs(
+            fn_input_parameters, parser_fn
+        )
+
+        # Call appropriate standardisation function with input parameters
+        data: list[MetadataAndData] = parser_fn(**parser_input_parameters)
+
+        # # TODO: Add in use of self.check_chunks() - used for Footprint only at the moment
+        # chunking_kwargs = self.create_schema_kwargs(chunking_params, fn_input_parameters, data[0])
+        # chunks = self.check_chunks(
+        #     ds=data[0].data,
+        #     chunks=chunks,
+        #     **chunking_kwargs
+        #     )
+        # if chunks:
+        #     logger.info(f"Rechunking with chunks={chunks}")
+
+        # Current workflow: if any datasource fails validation, whole filepath fails
+        for datasource in data:
+            validate_kwargs = self.create_schema_kwargs(validate_params, fn_input_parameters, datasource)
+
+            try:
+                self.validate_data_internal(datasource.data, **validate_kwargs)
+            except ValueError:
+                if isinstance(filepath, list):
+                    logger.error(
+                        f"Unable to validate and store data from grouped files: {', '.join([fp.name for fp in filepath])}."
+                    )
+                else:
+                    logger.error(
+                        f"Unable to validate and store data from file: {filepath.name}.",
+                    )
+                validated = False
+                break
+        else:
+            validated = True
+
+        if not validated:
+            return None
+
+        # Ensure the data is chunked
+        if chunks:
+            for datasource in data:
+                datasource.data = datasource.data.chunk(chunks)
+
+        self.align_metadata_attributes(data=data, update_mismatch=update_mismatch)
+
+        # Check to ensure no required keys are being passed through info_metadata dict
+        # before adding details
+        self.check_info_keys(info_metadata)
+        if info_metadata is not None:
+            additional_metadata.update(info_metadata)
+
+        # Mop up and add additional keys to metadata which weren't passed to the parser
+        data = self.update_metadata(data, additional_input_parameters, additional_metadata)
+
+        # Create Datasources, save them to the object store and get their UUIDs
+        data_type = self._data_type
+        datasource_uuids = self.assign_data(
+            data=data,
+            if_exists=if_exists,
+            new_version=new_version,
+            data_type=data_type,
+            compressor=compressor,
+            filters=filters,
+        )
+
+        for x in datasource_uuids:
+            if isinstance(filepath, str | Path):
+                x.update({"file": filepath.name})
+                logger.info(f"Completed processing: {filepath.name}.")
+            elif isinstance(filepath, list):
+                filepath_str = ", ".join([fp.name for fp in filepath])
+                x.update({"files": filepath_str})
+                logger.info(f"Completed processing files: {filepath_str}.")
+
+        return datasource_uuids
+
+    def read_file(
+        self,
+        filepath: multiPathType,
+        source_format: str,
+        if_exists: str = "auto",
+        save_current: str = "auto",
+        overwrite: bool = False,
+        force: bool = False,
+        compressor: Any | None = None,
+        filters: Any | None = None,
+        chunks: dict | None = None,
+        update_mismatch: str = "never",
+        open_files_grouped: bool = False,
+        info_metadata: dict | None = None,
+        **kwargs,
+    ) -> list[dict]:
+        """
+        Process files, standardise and store in the object store.
+        This function makes use of standardise parse functions to create consistent
+        internal data. This allows for data_type specific formatting dependent
+        on the expected inputs.
+
+        Args:
+            filepath: Filepath or set of filepaths to load and standardise,
+            source_format: Name of associated format for the provide filepath.
+            if_exists: What to do if existing data is present.
+                - "auto" - checks new and current data for timeseries overlap
+                   - adds data if no overlap
+                   - raises DataOverlapError if there is an overlap
+                - "new" - just include new data and ignore previous
+                - "combine" - replace and insert new data into current timeseries
+            save_current: Whether to save data in current form and create a new version.
+                - "auto" - this will depend on if_exists input ("auto" -> False), (other -> True)
+                - "y" / "yes" - Save current data exactly as it exists as a separate (previous) version
+                - "n" / "no" - Allow current data to updated / deleted
+            overwrite: Deprecated. This will use options for if_exists="new".
+            force: Force adding of data even if this is identical to data stored.
+            compressor: A custom compressor to use. If None, this will default to
+                `Blosc(cname="zstd", clevel=5, shuffle=Blosc.SHUFFLE)`.
+            See https://zarr.readthedocs.io/en/stable/api/codecs.html for more information on compressors.
+            filters: Filters to apply to the data on storage, this defaults to no filtering. See
+                https://zarr.readthedocs.io/en/stable/tutorial.html#filters for more information on picking filters
+            chunks: Chunking schema to use when storing data. It expects a dictionary of dimension name and chunk size,
+                for example {"time": 100}. If None then a chunking schema will be set automatically by OpenGHG.
+                See documentation for guidance on chunking: https://docs.openghg.org/tutorials/local/Adding_data/Adding_ancillary_data.html#chunking.
+                To disable chunking pass in an empty dictionary.
+            update_mismatch: This determines how mismatches between the internal data
+                "attributes" and the supplied / derived "metadata" are handled.
+                This includes the options:
+                    - "never" - don't update mismatches and raise an AttrMismatchError
+                    - "from_source" / "attributes" - update mismatches based on input data (e.g. data attributes)
+                    - "from_definition" / "metadata" - update mismatches based on associated data (e.g. site_info.json)
+            open_files_grouped: bool = False,
+            info_metadata: Allows to pass in additional tags to describe the data. e.g {"comment":"Quality checks have been applied"}
+            **kwargs: Specific keywords associated with the data type. See
+                the openghg.standardise.standardise_* functions for details
+                of what keywords are expected for this.
+        Returns:
+            list[dict]: Details of the datasource uuids for the processed files.
+        """
+
+        from openghg.store.spec import check_parser
+        from openghg.util import (
+            check_if_need_new_version,
+        )
+
+        # Check source format
+        source_format = check_parser(self._data_type, source_format)
+
+        # Check versioning details
+        if overwrite and if_exists == "auto":
+            logger.warning(
+                "Overwrite flag is deprecated in preference to `if_exists` (and `save_current`) inputs."
+                "See documentation for details of these inputs and options."
+            )
+            if_exists = "new"
+
+        # Making sure new version will be created by default if force keyword is included.
+        if force and if_exists == "auto":
+            if_exists = "new"
+
+        new_version = check_if_need_new_version(if_exists, save_current)
+
+        # Format input parameters (specific to data_type)
+        kwargs["filepath"] = filepath
+        kwargs["source_format"] = source_format
+        fn_input_parameters, additional_metadata = self.format_inputs(**kwargs)
+
+        # TODO: Do we pass filepath to format_inputs or treat separately?
+        filepaths = fn_input_parameters.get("filepaths")
+        if filepaths is None:
+            filepaths = fn_input_parameters["filepath"]
+
+        # Check hashes of previous files (included after any filepath(s) formatting)
+        _, unseen_hashes = self.check_hashes(filepaths=filepaths, force=force)
+
+        if not unseen_hashes:
+            return [{}]
+
+        filepaths = list(unseen_hashes.values())
+
+        if not filepaths:
+            return [{}]
+
+        if chunks is None:
+            chunks = {}
+
+        # Check if the files are opened grouped (e.g. for netcdf files)
+        if open_files_grouped:
+            results = self._standardise_from_file(
+                filepath=filepath,
+                fn_input_parameters=fn_input_parameters,
+                source_format=source_format,
+                update_mismatch=update_mismatch,
+                if_exists=if_exists,
+                new_version=new_version,
+                compressor=compressor,
+                filters=filters,
+                chunks=chunks,
+                info_metadata=info_metadata,
+                additional_metadata=additional_metadata,
+            )
+
+            return results
+
+        # If not, loop over multiple filepaths when present
+        loop_params = self.define_loop_params()
+
+        results: list[dict] = []
+
+        for i, filepath in enumerate(filepaths):
+
+            fn_input_parameters["filepath"] = filepath
+            if loop_params:
+                for key1, key2 in loop_params.items():
+                    fn_input_parameters[key1] = fn_input_parameters[key2][i]
+
+            datasource_uuids = self._standardise_from_file(
+                filepath=filepath,
+                fn_input_parameters=fn_input_parameters,
+                source_format=source_format,
+                update_mismatch=update_mismatch,
+                if_exists=if_exists,
+                new_version=new_version,
+                compressor=compressor,
+                filters=filters,
+                chunks=chunks,
+                info_metadata=info_metadata,
+                additional_metadata=additional_metadata,
+            )
+
+            results.extend(datasource_uuids)
+
+        self.store_hashes(unseen_hashes)
+
+        return results
+
+    def create_schema_kwargs(self, schema_params, fn_input_parameters, datasource) -> dict:
+        """
+        Create the keyword arguments needed when creating a data type schema.
+
+        Each data_type class has an associated schema which is used to validate that data
+        returned for the parse_* functions matches to our expected internal format.
+        For some data_type classes this requires some additional keywords to define the
+        appropriate schema. This function is to create the kwargs from the appropriate data.
+
+        This will look for the appropriate values first within the formatted user inputs
+        (fn_input_parameters) and then within the metadata of the created datasource.
+        This order of preference is mainly used to account for boolean values which
+        are turned into strings to be stored within the metadata.
+
+        Args:
+            schema_params: Parameters which we need to create the schema
+            fn_input_parameters: Input parameters which have been provided by the user / defaults.
+            datasource: Produced datasource which contains data and metadata.
+        Returns:
+            dict: Keyword arguments for the data type to create the schema
+        """
+
+        kwargs = {}
+
+        sources = [fn_input_parameters, datasource.metadata]
+
+        for key in schema_params:
+            for source in sources:
+                try:
+                    value = source[key]
+                except KeyError:
+                    continue
+                else:
+                    kwargs[key] = value
+                    break
+
+        return kwargs
+
+    def find_data_schema_inputs(self) -> list:
+        """
+        Extract the expected inputs for the validate_data method.
+        """
+        from openghg.util import find_function_inputs
+
+        fn = self.validate_data
+        inputs = find_function_inputs(fn)
+        inputs.remove("data")
+
+        return inputs
+
+    def find_chunking_schema_inputs(self) -> list:
+        """
+        Extract the expected inputs for the chunking_schema method.
+        """
+        from openghg.util import find_function_inputs
+
+        fn = self.chunking_schema
+        inputs = find_function_inputs(fn)
+
+        return inputs
 
     def store_data(self, *args: Any, **kwargs: Any) -> list[dict] | None:
         raise NotImplementedError
@@ -327,35 +686,16 @@ class BaseStore:
 
         return list_keys
 
-    def read_file(
-        self,
-        filepath: multiPathType,
-        update_mismatch: str = "never",
-        if_exists: str = "auto",
-        save_current: str = "auto",
-        overwrite: bool = False,
-        force: bool = False,
-        compressor: Any | None = None,
-        filters: Any | None = None,
-        chunks: dict | None = None,
-        info_metadata: dict | None = None,
-        **kwargs,
-    ):
-        
-        # STEP 1: Check source format
-        # STEP 2?: Set versioning details
-        # DATA_TYPE STEP: Format metadata
-        # Specific details may need to be supplied:
-        #   - validate_data
-        #   - looping over filepath
-        #   - looping over other metadata keys
-        #   - "Type-specific settings?"
-        # STEP 3?: Check hashes
-        # STEP 4: Standardise data (looping style)
-        # STEP 5: Validate data
-        # STEP 6: Store data
+    def align_metadata_attributes(self, data, update_mismatch) -> None:
+        """Default to returning None for cases where this method isn't
+        defined yet within the child data_type class.
+        """
+        logger.warning("Align metadata attributes is not implemented for this data type")
+        return None
 
-        pass
+    def define_loop_params(self) -> dict:
+        """Default to returning an empty dict if there are no loop parameters."""
+        return {}
 
     def assign_data(
         self,
