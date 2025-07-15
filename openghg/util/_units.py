d8b98b78e247122229c54795477acbda06d81b14
SutarPrasad <vq21425@bristol.ac.uk>
2025-07-15 13:05:05 +0100

    assign_units function util

--- openghg/util/_units.py
+++ openghg/util/_units.py
@@ -1,4 +1,5 @@
 import logging
+import cf_xarray.units
 import pint
 import pint_xarray
 from openghg.util._file import load_internal_json
@@ -12,8 +13,17 @@ def _openghg_unit_registry() -> pint.UnitRegistry:
     ureg.define("ppt = 1e-12 mol/mol= parts_per_trillion")
     ureg.define("permeg = 0.001 permille")
     ureg.define("m2 = m*m = metres_squared")
-    ureg.define("hPa = hectopascal")
+    ureg.define("hpa = hectopascal")
+    ureg.define("degrees_north = degree")
+    ureg.define("degrees_east = degree")
+    ureg.define("degrees_west = degree")
+    ureg.define("degrees_south = degree")
+    ureg.define("Degrees_north = degree")
+    ureg.define("Degrees_east = degree")
+    ureg.define("Degrees_west = degree")
+    ureg.define("Degrees_south = degree")
     return ureg
+
 
 def _normalize_unit(unit_str: str) -> str:
     substitutions = {
@@ -24,125 +34,9 @@ def _normalize_unit(unit_str: str) -> str:
     }
     return substitutions.get(unit_str, unit_str)
 
+
 def _read_attributes_json() -> dict:
     return load_internal_json("attributes.json")
-
-
-# def _assign_obs_units(data, target_units: dict | None = None):
-#     ureg = _openghg_unit_registry()
-#     pint_xarray.accessors.default_registry = ureg
-
-#     attrs = _read_attributes_json()
-#     unit_mapping = attrs["unit_pint"]
-#     non_standard = attrs["unit_non_standard_interpret"]
-
-#     for key in data.data.keys():
-#         if "units" in data.data[key].attrs:
-#             i_unit = data.data[key].attrs["units"]
-
-#             # Determine appropriate pint unit from mappings
-#             if i_unit in unit_mapping:
-#                 unit = unit_mapping[i_unit]
-#             elif i_unit == "mol/mol":
-#                 unit = i_unit
-#             elif i_unit in non_standard:
-#                 unit = non_standard[i_unit]
-#             else:
-#                 raise ValueError(f"The {key} with unit {i_unit} is not recognised.")
-
-#             # Quantify the data with inferred unit
-#             data.data[key] = data.data[key].pint.quantify(ureg.parse_units(unit))
-
-#             # Only convert if target unit provided
-#             if target_units and key in target_units:
-#                 data.data[key] = data.data[key].pint.to(target_units[key])
-
-#     return data
-
-
-# def _assign_footprint_units(data, target_units: dict | None = None):
-#     ureg = _openghg_unit_registry()
-#     pint_xarray.accessors.default_registry = ureg
-
-#     for key in data.data.keys():
-#         if "units" in data.data[key].attrs:
-#             # Special handling for ambiguous units
-#             if key == "air_temperature":
-#                 i_unit = "degC"
-#             elif key == "wind_speed":
-#                 i_unit = "m/s"
-#             else:
-#                 i_unit = data.data[key].attrs["units"]
-
-#             try:
-#                 for coord in data.data[key].coords:
-#                     data.data[key][coord].attrs.pop("units", None)
-#                 data.data[key] = data.data[key].pint.quantify(ureg.parse_units(i_unit))
-
-#                 # Convert to user-specified unit if provided
-#                 if target_units and key in target_units:
-#                     data.data[key] = data.data[key].pint.to(target_units[key])
-
-#             except pint.errors.UndefinedUnitError:
-#                 print(f"Skipping {key} as pint could not parse {i_unit}")
-
-#     return data
-
-
-# def _assign_flux_units(data, target_units: dict | None = None):
-#     ureg = _openghg_unit_registry()
-#     pint_xarray.accessors.default_registry = ureg
-
-#     attrs = _read_attributes_json()
-#     non_standard = attrs["unit_non_standard_interpret"]
-
-#     flux_unit = data.data.flux.attrs.get("units", "mol/m^2/s")  # Use default if missing
-
-#     if flux_unit in non_standard:
-#         flux_unit = non_standard[flux_unit]
-
-#     data.data["flux"] = data.data["flux"].pint.quantify(ureg.parse_units(flux_unit))
-
-#     if target_units and "flux" in target_units:
-#         data.data["flux"] = data.data["flux"].pint.to(target_units["flux"])
-
-#     return data
-
-
-
-# def _assign_bc_units(data):
-#     ureg = _openghg_unit_registry()
-#     pint_xarray.accessors.default_registry = ureg
-
-#     for direction in ["n", "e", "s", "w"]:
-#         var = f"vmr_{direction}"
-#         try:
-#             bc_unit = data.data[var].attrs["units"]
-#         except KeyError:
-#             print(f"No units provided for {var}. Assuming units of mol/mol")
-#             bc_unit = "mol/mol"
-#         data.data[var] = data.data[var].pint.quantify(ureg.parse_units(bc_unit))
-
-#     return data
-
-
-# def assign_units(
-#     data, target_units
-# ):
-#     """
-#     Assign pint units to OpenGHG data objects based on their type.
-#     """
-#     if type(data).__name__ == "ObsData" or type(data).__name__ == "ObsColumnData":
-#         return _assign_obs_units(data=data, target_units=target_units)
-#     elif type(data).__name__ == "FootprintData":
-#         return _assign_footprint_units(data)
-#     elif type(data).__name__ == "FluxData":
-#         return _assign_flux_units(data)
-#     elif type(data).__name__ == "BoundaryConditionsData":
-#         return _assign_bc_units(data)
-#     else:
-#         raise TypeError("Unsupported data type for unit assignment.")
-
 
 
 def assign_units(data, target_units=None):
@@ -153,9 +47,9 @@ def assign_units(data, target_units=None):
     unit_mapping = attrs["unit_pint"]
     non_standard = attrs["unit_non_standard_interpret"]
 
-    for key in data.data.keys():
+    for key in data.data.data_vars:
         if "units" in data.data[key].attrs:
-            i_unit = data.data[key].attrs["units"]
+            i_unit = data.data[key].attrs["units"].lower()
             i_unit = _normalize_unit(i_unit)
             try:
                 if i_unit in unit_mapping:
@@ -165,12 +59,18 @@ def assign_units(data, target_units=None):
                 else:
                     unit = i_unit   
 
-                data.data[key] = data.data[key].pint.quantify(ureg.parse_units(unit))
-
+                quanitified_data = data.data[key].pint.quantify(ureg.parse_units(unit))
+                
+                
                 if target_units and key in target_units:
-                    data.data[key] = data.data[key].pint.to(target_units[key])
+                    quanitified_data = quanitified_data.pint.to(target_units[key])
 
-            except pint.errors.UndefinedUnitError:
+                data.data[key] = quanitified_data
+                # Added the original attrs unit value back to the data variable on a pint quantified dataset, as the entire workflow is not yet fetching units via data[key].pint.units. 
+                cf_unit = f"{quanitified_data.pint.units:cf}"
+                data.data[key].attrs["units"] = i_unit
+
+            except pint.errors.UndefinedUnitError or pint.errors.is_valid_unit_name():
                 logger.warning(f"The unit '{i_unit}' for key '{key}' is not recognised by mappings or Pint.")
 
     return data  
