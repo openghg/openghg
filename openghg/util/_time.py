3180689354fb23ff653414247d80abf6d49887dc
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2025-07-21 14:47:33 +0100

    Checking timestamps > 3 first in infer_frequency and returning pandas.infer_freq output

--- openghg/util/_time.py
+++ openghg/util/_time.py
@@ -872,6 +872,9 @@ def infer_frequency(timestamps: DatetimeIndex) -> str | None:
 
     timestamps = timestamps.sort_values()
 
+    if len(timestamps) >= 3:
+        return pd.infer_freq(timestamps)
+
     if len(timestamps) < 2:
         raise ValueError("Unable to infer frequency from <2 data points")
     elif len(timestamps) == 2:
@@ -910,8 +913,6 @@ def infer_frequency(timestamps: DatetimeIndex) -> str | None:
                     break
             else:
                 inferred_period = f"{(total_seconds / day):.1f}D"
-    else:
-        inferred_period = pd.infer_freq(timestamps)
 
     return inferred_period
 
