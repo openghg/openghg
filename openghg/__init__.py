1e66c6d6e309012019604559263b1c7090fb3b32
SutarPrasad <vq21425@bristol.ac.uk>
2025-08-27 17:48:26 +0100

    version

--- openghg/__init__.py
+++ openghg/__init__.py
@@ -76,3 +76,7 @@ __version__ = _version.get_versions()["version"]
 from . import _version
 
 __version__ = _version.get_versions()["version"]
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
