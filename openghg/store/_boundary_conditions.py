bebeca82932b01be5f88976cfdca1541c1e87e82
SutarPrasad <vq21425@bristol.ac.uk>
2025-10-16 10:01:56 +0100

    black formatted

--- openghg/store/_boundary_conditions.py
+++ openghg/store/_boundary_conditions.py
@@ -177,7 +177,6 @@ class BoundaryConditions(BaseStore):
             fn_input_parameters, parser_fn
         )
 
-
         # Call appropriate standardisation function with input parameters
         try:
             bc_data = parser_fn(**parser_input_parameters)
