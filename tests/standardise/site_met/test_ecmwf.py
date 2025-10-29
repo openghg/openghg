75cece0ccb618ae54acf0070a2c22f77c80ade85
Elena Fillola <ef17148@bristol.ac.uk>
2025-10-29 14:02:10 +0000

    added met parser test

---
+++ tests/standardise/site_met/test_ecmwf.py
@@ -0,0 +1,54 @@
+import pytest
+import os
+from openghg.standardise.site_met import parse_ecmwf
+from helpers import get_met_datapath
+
+@pytest.mark.parametrize(
+    "site,network,filename",
+    [
+        (
+            "mhd",
+            "AGAGE",
+            "Met_mhd_agage_201608.nc",
+        ),
+        (
+            "tac",
+            "AGAGE",
+            "Met_tac_agage_201608.nc",
+        )]
+)
+
+def test_ecmwf_site_met(site, network, filename):
+
+    met_filepath = get_met_datapath(filename)
+
+    data  = parse_ecmwf(met_filepath, site, network)
+    dataset = data[0].data
+
+    # test dataset
+
+    assert dataset is not None
+
+
+    expected_dims = ["time", "lat", "lon", "pressure_level", "inlet_height"]
+
+    expected_vars = ["u_wind", "v_wind"]
+
+    existing_dims = dataset.variables.keys()
+    missing_dims = [var for var in expected_dims if var not in existing_dims]
+    assert not missing_dims, f"Missing dimensions in met file: {missing_dims}"
+
+    existing_vars = dataset.variables.keys()
+    missing_vars = [var for var in expected_vars if var not in existing_vars]
+    assert not missing_vars, f"Missing variables in footprint file: {missing_vars}"
+
+
+    assert len(dataset.inlet_height.values) == len(dataset.inlet_pressure.values)
+
+    # test metadata
+
+    metadata = data[0].metadata
+
+    assert metadata["site"] == site
+    assert metadata["network"] == network
+    assert metadata["source"] == "ECMWF ERA5"
