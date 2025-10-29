5fc039b2e14db3af9df563b9a0d05adce07b0849
Elena Fillola <ef17148@bristol.ac.uk>
2025-10-29 14:52:10 +0000

    deleting metstore and references

--- openghg/store/__init__.py
+++ openghg/store/__init__.py
@@ -10,7 +10,6 @@ from ._obssurface import ObsSurface
 from ._populate import add_noaa_obspack
 from ._met import SiteMet
 
-# from ._metstore import METStore
 from ._meta import data_class_info, get_data_class
 from ._flux_timeseries import FluxTimeseries
 from ._metakeys_config import (
