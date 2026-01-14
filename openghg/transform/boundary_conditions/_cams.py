b7d39e4674ca9d8d9ae6ac582a24f45f6ad0d846
SutarPrasad <vq21425@bristol.ac.uk>
2026-01-14 13:14:58 +0000

    raising error

--- openghg/transform/boundary_conditions/_cams.py
+++ openghg/transform/boundary_conditions/_cams.py
@@ -32,14 +32,13 @@ def get_cams_data_units(ds: xr.DataArray, species: str) -> str:
     """
     species = species.upper()
     if species not in ds:
-        raise KeyError(f"Species '{species}' not found in dataset.")
+        raise ValueError(f"Species '{species}' not found in dataset.")
     try:
         units_from_data = ds[species].attrs["units"]
         parsed_units = cf_ureg.parse_expression(units_from_data)
         if not parsed_units.dimensionless:
-            units = str(parsed_units.units)
-        else:
-            units = str(parsed_units.magnitude)
+            raise ValueError(f"Invalid units for '{species}'.  Not dimensionless: {units_from_data}")
+        units = str(parsed_units.magnitude)
     except Exception as e:
         raise ValueError(f"Could not parse units: {units_from_data}") from e
     return units
