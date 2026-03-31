e4be22cbb9dc2903c672c5e58582e02dbc1831c5
SutarPrasad <vq21425@bristol.ac.uk>
2026-03-31 12:34:38 +0100

    updates to open_nc

--- openghg/util/_file.py
+++ openghg/util/_file.py
@@ -336,6 +336,8 @@ def open_nc_fn(
     realign_on_domain: str | None = None,
     sel_month: bool = False,
     check_coords: str | None = None,
+    *args, **kwargs
+
 ) -> tuple[Callable, str | Path | list[str] | list[Path]]:
     """
     Check the filepath input to choose which xarray open function to use:
@@ -376,14 +378,15 @@ def open_nc_fn(
 
     if isinstance(filepath, list):
         if len(filepath) > 1:
-            xr_open_fn_1: Callable = partial(xr.open_mfdataset, preprocess=process)
+            xr_open_fn_1: Callable = partial(xr.open_mfdataset, preprocess=process,*args, **kwargs
+)
             return xr_open_fn_1, filepath
 
         else:
             filepath = filepath[0]
 
     def xr_open_fn_2(x: pathType) -> xr.DataArray | xr.Dataset:
-        return process(xr.open_dataset(x))
+        return process(xr.open_dataset(x,*args, **kwargs))
 
     return xr_open_fn_2, filepath
 
@@ -393,6 +396,7 @@ def open_time_nc_fn(
     realign_on_domain: str | None = None,
     sel_month: bool = False,
     check_coords: str | None = "time",
+    *args, **kwargs
 ) -> tuple[Callable, str | Path | list[str] | list[Path]]:
     """
     Check the filepath input to choose which xarray open function to use:
@@ -406,6 +410,7 @@ def open_time_nc_fn(
         realign_on_domain=realign_on_domain,
         sel_month=sel_month,
         check_coords=check_coords,
+        *args, **kwargs
     )
 
 
