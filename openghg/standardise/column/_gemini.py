ff56d620c00198ce49c040307c57bd10f36090da
SutarPrasad <vq21425@bristol.ac.uk>
2026-04-13 16:53:45 +0100

    blck

--- openghg/standardise/column/_gemini.py
+++ openghg/standardise/column/_gemini.py
@@ -33,7 +33,7 @@ def _filter_and_resample(ds: xr.Dataset, species: str, quality_filt: bool, resam
     """
     if quality_filt:
         logger.info("Applying filter based on variable 'qual_flag'.")
-        mask = (ds.qual_flag == 1).compute() 
+        mask = (ds.qual_flag == 1).compute()
         ds = ds.where(mask, drop=True)
     ds = ds.dropna("time").sortby("time")
 
