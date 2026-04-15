21f00b7732e2073cd3a77c09914139fe88c0000b
SutarPrasad <vq21425@bristol.ac.uk>
2026-04-15 12:28:19 +0100

    blk change

--- openghg/standardise/column/_gemini.py
+++ openghg/standardise/column/_gemini.py
@@ -29,12 +29,16 @@ def _preprocess(ds: xr.Dataset, quality_filter: bool) -> xr.Dataset:
     ds = ds.compute()
 
     if not quality_filter:
-        logger.warning("It is preferred to use the quality filter (qual_flag == 1). Returning unfiltered data.")
+        logger.warning(
+            "It is preferred to use the quality filter (qual_flag == 1). Returning unfiltered data."
+        )
     else:
         # Filter to qual_flag == 1 and drop timesteps where all vars are NaN
         ds = ds.where(ds["qual_flag"] == 1, drop=True).dropna(dim="time", how="all")
         if ds.time.size == 0:
-            raise ValueError("Dataset is empty after filtering. Nothing to store. Possibly all the data have qual_flag != 1.")
+            raise ValueError(
+                "Dataset is empty after filtering. Nothing to store. Possibly all the data have qual_flag != 1."
+            )
 
     for var in ds.data_vars:
         if "time" not in ds[var].dims and var not in ["longitude", "latitude", "obs_height"]:
@@ -125,13 +129,11 @@ def parse_gemini(
     # Here we assume that all the files have the same variables and that they are in the same order
 
     data = xr.open_mfdataset(
-            filepath,
-            combine="by_coords",
-            preprocess=partial(_preprocess,quality_filter=quality_filter),
-            decode_times=False,
-        )[
-            var_to_read
-        ].chunk(chunks if chunks is not None else {})
+        filepath,
+        combine="by_coords",
+        preprocess=partial(_preprocess, quality_filter=quality_filter),
+        decode_times=False,
+    )[var_to_read].chunk(chunks if chunks is not None else {})
 
     # Create metadata #
     attributes = cast(MutableMapping, data.attrs)
