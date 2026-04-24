66f9a99ac8f208f82b27dcd6c590ef27dda82d88
SutarPrasad <vq21425@bristol.ac.uk>
2026-04-24 10:30:39 +0100

    reinde inside _make_high_freq_flux

--- openghg/analyse/_modelled_obs.py
+++ openghg/analyse/_modelled_obs.py
@@ -183,6 +183,9 @@ def _make_high_freq_flux(flux: xr.DataArray, fp: xr.DataArray | xr.Dataset) -> x
     # create rolling windows
     flux_high_freq = _make_hf_flux_rolling_avg_array(flux_high_freq, fp)
 
+    # reindex to align with fp time coordinates (after creating rolling windows to avoid NaN values at start of time series)
+    flux_high_freq = flux_high_freq.reindex({"time": fp.time}, method="ffill")
+    
     flux_high_freq.attrs["units"] = flux.attrs.get("units")
 
     return flux_high_freq
@@ -234,7 +237,7 @@ def fp_x_flux_time_resolved(
 
     # create high frequency flux (resampled to gcd of footprint time and H_back frequencies) with H_back dim
     flux_high_freq = _make_high_freq_flux(flux, fp)
-    flux_high_freq = flux_high_freq.reindex({"time": fp_time_resolved.time}, method="ffill")
+    
     fp_x_flux = (flux_high_freq.pint.quantify() * fp_time_resolved.pint.quantify()).sum(
         "H_back"
     ) + fp_x_flux_residual
