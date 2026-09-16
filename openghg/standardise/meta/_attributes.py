759c4bedb5a457553f0a21fa55a8bffacfda7776
Rachel Tunnicliffe <rt17603@bristol.ac.uk>
2026-09-16 16:03:47 +0100

    Redefining how 'file_created' attribute is created to check and update 'history' if this already exists and replace with new value for when data was added to object store (consistent with behaviour for surface)

--- openghg/standardise/meta/_attributes.py
+++ openghg/standardise/meta/_attributes.py
@@ -528,10 +528,12 @@ def get_flux_attributes(
     else:
         global_attributes["title"] = f"{source} emissions/flux of {species} for {domain} domain"
 
-    if "file_created" not in global_attributes:
+    if "file_created" in global_attributes:
+        history = global_attributes.get("history", "")
+        global_attributes["history"] = history + f" Previously processed: {global_attributes['file_created']}"
         global_attributes["file_created"] = str(timestamp_now())
-    if "process_by" not in global_attributes:
-        global_attributes["processed_by"] = "OpenGHG_Cloud"
+
+    global_attributes["processed_by"] = "OpenGHG_Cloud"
 
     species_label = define_species_label(species)
 
